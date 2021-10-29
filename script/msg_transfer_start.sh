8e24ab7c2780c813c94a070493144a52c0400e51
skiffer-git <72860476+skiffer-git@users.noreply.github.com>
2021-10-29 15:01:46 +0800

    Add files via upload
    update script

--- script/msg_transfer_start.sh
+++ script/msg_transfer_start.sh
@@ -8,31 +8,29 @@ source ./path_info.cfg
 #Check if the service exists
 #If it is exists,kill this process
 check=`ps aux | grep -w ./${msg_transfer_name} | grep -v grep| wc -l`
-if [ $check -eq 1 ]
+if [ $check -ge 1 ]
 then
 oldPid=`ps aux | grep -w ./${msg_transfer_name} | grep -v grep|awk '{print $2}'`
  kill -9 $oldPid
 fi
 #Waiting port recycling
 sleep 1
+
 cd ${msg_transfer_binary_root}
-    nohup ./${msg_transfer_name}  >>../logs/openIM.log 2>&1 &
+for ((i = 0; i < ${msg_transfer_service_num}; i++)); do
+      nohup ./${msg_transfer_name}  >>../logs/openIM.log 2>&1 &
+done
+
 #Check launched service process
 check=`ps aux | grep -w ./${msg_transfer_name} | grep -v grep| wc -l`
-if [ $check -eq 1 ]
+if [ $check -ge 1 ]
 then
 newPid=`ps aux | grep -w ./${msg_transfer_name} | grep -v grep|awk '{print $2}'`
-ports=`netstat -netulp | grep -w ${newPid}|awk '{print $4}'|awk -F '[:]' '{print $NF}'`
 allPorts=""
-
-for i in $ports ;
-do
-     allPorts=${allPorts}"$i "
-done
     echo -e ${SKY_BLUE_PREFIX}"SERVICE START SUCCESS "${COLOR_SUFFIX}
     echo -e ${SKY_BLUE_PREFIX}"SERVICE_NAME: "${COLOR_SUFFIX}${YELLOW_PREFIX}${msg_transfer_name}${COLOR_SUFFIX}
     echo -e ${SKY_BLUE_PREFIX}"PID: "${COLOR_SUFFIX}${YELLOW_PREFIX}${newPid}${COLOR_SUFFIX}
     echo -e ${SKY_BLUE_PREFIX}"LISTENING_PORT: "${COLOR_SUFFIX}${YELLOW_PREFIX}${allPorts}${COLOR_SUFFIX}
 else
-    echo -e ${YELLOW_PREFIX}${msg_transfer_name}${COLOR_SUFFIX}${RED_PREFIX}"SERVICE START ERROR !!! PLEASE CHECK ERROR LOG"${COLOR_SUFFIX}
+    echo -e ${YELLOW_PREFIX}${msg_transfer_name}${COLOR_SUFFIX}${RED_PREFIX}"SERVICE START ERROR, PLEASE CHECK openIM.log"${COLOR_SUFFIX}
 fi
