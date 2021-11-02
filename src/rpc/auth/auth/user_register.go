3297f61b09f787448b9efc520f97e9192dfd40fe
Xan <270084782@qq.com>
2021-11-02 11:06:45 +0800

    feat:uid generator & rm incompatible gopkg

--- src/rpc/auth/auth/user_register.go
+++ src/rpc/auth/auth/user_register.go
@@ -4,12 +4,16 @@ import (
 	"Open_IM/src/common/db/mysql_model/im_mysql_model"
 	"Open_IM/src/common/log"
 	pbAuth "Open_IM/src/proto/auth"
+	"Open_IM/src/utils"
 	"context"
 )
 
 func (rpc *rpcAuth) UserRegister(_ context.Context, pb *pbAuth.UserRegisterReq) (*pbAuth.UserRegisterResp, error) {
 	log.Info("", "", "rpc user_register start, [data: %s]", pb.String())
 
+	if len(pb.UID) == 0 {
+		pb.UID = utils.GenID()
+	}
 	if err := im_mysql_model.UserRegister(pb); err != nil {
 		log.Error("", "", "rpc user_register error, [data: %s] [err: %s]", pb.String(), err.Error())
 		return &pbAuth.UserRegisterResp{Success: false}, err
