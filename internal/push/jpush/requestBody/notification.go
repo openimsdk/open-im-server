d7f82196dce15f6ff05ca68aef60954e3500b2dc
Gordon <1432970085@qq.com>
2021-11-12 18:15:28 +0800

    push modify

--- internal/push/jpush/requestBody/notification.go
+++ internal/push/jpush/requestBody/notification.go
@@ -3,12 +3,13 @@ package requestBody
 import "Open_IM/pkg/common/config"
 
 type Notification struct {
-	Alert   string   `json:"alert,omitempty"`
-	Android *Android `json:"android,omitempty"`
-	IOS     *Ios     `json:"ios,omitempty"`
+	Alert   string  `json:"alert,omitempty"`
+	Android Android `json:"android,omitempty"`
+	IOS     Ios     `json:"ios,omitempty"`
 }
 
 type Android struct {
+	Alert  string `json:"alert,omitempty"`
 	Intent struct {
 		URL string `json:"url,omitempty"`
 	} `json:"intent,omitempty"`
@@ -18,6 +19,7 @@ type Ios struct {
 
 func (n *Notification) SetAlert(alert string) {
 	n.Alert = alert
+	n.Android.Alert = alert
 }
 func (n *Notification) SetAndroidIntent() {
 	n.Android.Intent.URL = config.Config.Push.Jpns.PushIntent
