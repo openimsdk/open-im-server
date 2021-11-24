99da85a39c1016ac0b8229d5f2395a01ef45848c
wenxu12345 <44203734@qq.com>
2021-11-24 10:10:40 +0800

    sleep 10 15

--- script/docker_start_all.sh
+++ script/docker_start_all.sh
@@ -12,13 +12,14 @@ need_to_start_server_shell=(
   timer_start.sh
 )
 
+#fixme The 10 second delay to start the project is for the docker-compose one-click to start openIM when the infrastructure dependencies are not started
+sleep 10
 
 for i in ${need_to_start_server_shell[*]}; do
   chmod +x $i
   ./$i
 done
 
-#fixme The 15 second delay to start the project is for the docker-compose one-click to start openIM when the infrastructure dependencies are not started
 sleep 15
 
 #fixme prevents the openIM service exit after execution in the docker container
