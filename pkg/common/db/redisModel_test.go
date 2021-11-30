f18f54afc6e453a7cd5cf8028f0e9d01c110cae1
Gordon <1432970085@qq.com>
2021-11-30 19:35:55 +0800

    test modify

---
+++ pkg/common/db/redisModel_test.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_SetTokenMapByUidPid(t *testing.T) {
+	m := make(map[string]int, 0)
+	m["哈哈"] = 1
+	m["heihei"] = 2
+	_ = DB.SetTokenMapByUidPid("1234", 2, m)
+
+}
+func Test_GetTokenMapByUidPid(t *testing.T) {
+	m, err := DB.GetTokenMapByUidPid("1234", "Android")
+	assert.Nil(t, err)
+	fmt.Println(m)
+}
