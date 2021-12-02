6d1c70b8d3380101bb9e214b4f19b3557a8b3f1e
wenxu12345 <44203734@qq.com>
2021-12-02 11:27:06 +0800

    get online state by secret

--- internal/api/user/get_user_info.go
+++ internal/api/user/get_user_info.go
@@ -2,8 +2,10 @@ package user
 
 import (
 	"Open_IM/pkg/common/config"
+	"Open_IM/pkg/common/constant"
 	"Open_IM/pkg/common/log"
 	"Open_IM/pkg/grpc-etcdv3/getcdv3"
+	pbRelay "Open_IM/pkg/proto/relay"
 	pbUser "Open_IM/pkg/proto/user"
 	"context"
 	"github.com/gin-gonic/gin"
@@ -20,6 +22,72 @@ type userInfo struct {
 	Birth  string `json:"birth"`
 	Email  string `json:"email"`
 	Ex     string `json:"ex"`
+}
+
+type paramsGetUsersOnlineStatus struct {
+	OperationID string   `json:"operationID" binding:"required"`
+	UserIDList  []string `json:"userIDList" binding:"required,lte=200"`
+	Secret      string   `json:"secret" binding:"required,max=32"`
+}
+
+func GetUsersOnlineStatus(c *gin.Context) {
+	params := paramsGetUsersOnlineStatus{}
+	if err := c.BindJSON(&params); err != nil {
+		log.NewError(params.OperationID, "bind json failed ", err.Error(), c)
+		c.JSON(http.StatusBadRequest, gin.H{"errCode": 400, "errMsg": err.Error()})
+		return
+	}
+
+	if params.Secret != config.Config.Secret {
+		log.NewError(params.OperationID, "parse token failed ", params.Secret, config.Config.Secret)
+		c.JSON(http.StatusBadRequest, gin.H{"errCode": 401, "errMsg": "secret failed"})
+		return
+	}
+
+	req := &pbRelay.GetUsersOnlineStatusReq{
+		OperationID: params.OperationID,
+		UserIDList:  params.UserIDList,
+	}
+	var wsResult []*pbRelay.GetUsersOnlineStatusResp_SuccessResult
+	var respResult []*pbRelay.GetUsersOnlineStatusResp_SuccessResult
+	flag := false
+	log.NewDebug(params.OperationID, "GetUsersOnlineStatus req come here", params.UserIDList)
+
+	grpcCons := getcdv3.GetConn4Unique(config.Config.Etcd.EtcdSchema, strings.Join(config.Config.Etcd.EtcdAddr, ","), config.Config.RpcRegisterName.OpenImOnlineMessageRelayName)
+	for _, v := range grpcCons {
+		client := pbRelay.NewOnlineMessageRelayServiceClient(v)
+		reply, err := client.GetUsersOnlineStatus(context.Background(), req)
+		if err != nil {
+			log.NewError(params.OperationID, "GetUsersOnlineStatus rpc  err", req.String(), err.Error())
+			continue
+		} else {
+			if reply.ErrCode == 0 {
+				wsResult = append(wsResult, reply.SuccessResult...)
+			}
+		}
+	}
+	log.NewDebug(params.OperationID, "call GetUsersOnlineStatus rpc server is success", wsResult)
+	//Online data merge of each node
+	for _, v1 := range params.UserIDList {
+		flag = false
+		temp := new(pbRelay.GetUsersOnlineStatusResp_SuccessResult)
+		for _, v2 := range wsResult {
+			if v2.UserID == v1 {
+				flag = true
+				temp.UserID = v1
+				temp.Status = constant.OnlineStatus
+				temp.DetailPlatformStatus = append(temp.DetailPlatformStatus, v2.DetailPlatformStatus...)
+			}
+		}
+		if !flag {
+			temp.UserID = v1
+			temp.Status = constant.OfflineStatus
+		}
+		respResult = append(respResult, temp)
+	}
+	log.NewDebug(params.OperationID, "Finished merged data", respResult)
+	resp := gin.H{"errCode": 0, "errMsg": "", "successResult": respResult}
+	c.JSON(http.StatusOK, resp)
 }
 
 func GetUserInfo(c *gin.Context) {
