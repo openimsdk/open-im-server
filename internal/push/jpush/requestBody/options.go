170310e23c0bab12462b8fa0644e7002fa882b39
Gordon <1432970085@qq.com>
2021-12-09 18:15:40 +0800

    push add ios push

--- internal/push/jpush/requestBody/options.go
+++ internal/push/jpush/requestBody/options.go
@@ -1,7 +1,7 @@
 package requestBody
 
 type Options struct {
-	ApnsProduction bool `json:"apns_production,omitempty"`
+	ApnsProduction bool `json:"apns_production"`
 }
 
 func (o *Options) SetApnsProduction(c bool) {
