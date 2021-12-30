1eee8f439bd80111ce9868f90b81b72be2f7ca5d
wenxu12345 <44203734@qq.com>
2021-12-30 12:30:30 +0800

    Refactor code

--- internal/api/group/group.go
+++ internal/api/group/group.go
@@ -3,8 +3,6 @@ package group
 import (
 	api "Open_IM/pkg/base_info"
 	"Open_IM/pkg/common/config"
-	"Open_IM/pkg/common/constant"
-	imdb "Open_IM/pkg/common/db/mysql_model/im_mysql_model"
 	"Open_IM/pkg/common/log"
 	"Open_IM/pkg/common/token_verify"
 	"Open_IM/pkg/grpc-etcdv3/getcdv3"
