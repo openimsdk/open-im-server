0c02afa54eaa9275629fe9c9133069d3b6770a9f
wenxu12345 <44203734@qq.com>
2021-12-30 17:34:54 +0800

    Refactor code

--- cmd/open_im_api/main.go
+++ cmd/open_im_api/main.go
@@ -56,7 +56,7 @@ func main() {
 		groupRouterGroup.POST("/get_group_applicationList", group.GetGroupApplicationList)   //1
 		groupRouterGroup.POST("/get_groups_info", group.GetGroupsInfo)                       //1
 		groupRouterGroup.POST("/kick_group", group.KickGroupMember)                          //1
-		groupRouterGroup.POST("/get_group_member_list", group.GetGroupMemberList)
+		groupRouterGroup.POST("/get_group_member_list", group.GetGroupMemberList)            //no use
 		groupRouterGroup.POST("/get_group_all_member_list", group.GetGroupAllMember)
 		groupRouterGroup.POST("/get_group_members_info", group.GetGroupMembersInfo)
 		groupRouterGroup.POST("/invite_user_to_group", group.InviteUserToGroup) //1
