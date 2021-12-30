6d0f454e6aede1583ff5e05fd5d7dceeff13439e
wenxu12345 <44203734@qq.com>
2021-12-30 14:17:29 +0800

    Refactor code

--- cmd/open_im_api/main.go
+++ cmd/open_im_api/main.go
@@ -55,7 +55,7 @@ func main() {
 		groupRouterGroup.POST("/transfer_group", group.TransferGroupOwner)                   //1
 		groupRouterGroup.POST("/get_group_applicationList", group.GetGroupApplicationList)   //1
 		groupRouterGroup.POST("/get_groups_info", group.GetGroupsInfo)                       //1
-		groupRouterGroup.POST("/kick_group", group.KickGroupMember)
+		groupRouterGroup.POST("/kick_group", group.KickGroupMember)                          //1
 		groupRouterGroup.POST("/get_group_member_list", group.GetGroupMemberList)
 		groupRouterGroup.POST("/get_group_all_member_list", group.GetGroupAllMember)
 		groupRouterGroup.POST("/get_group_members_info", group.GetGroupMembersInfo)
