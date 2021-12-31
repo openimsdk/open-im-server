1290bc8cc9f30b6372bf99a6701e1acf94d06495
wenxu12345 <44203734@qq.com>
2021-12-31 10:45:11 +0800

    Refactor code

--- pkg/common/db/mysql_model/im_mysql_model/user_black_list_model.go
+++ pkg/common/db/mysql_model/im_mysql_model/user_black_list_model.go
@@ -40,7 +40,7 @@ func RemoveBlackList(ownerUserID, blockUserID string) error {
 		return err
 	}
 
-	err = dbConn.Table("user_black_list").Where("owner_user_id=? and block_user_id=?", ownerUserID, blockUserID).Delete(&Black{}).Error
+	err = dbConn.Table("user_black_list").Where("owner_user_id=? and block_user_id=?", ownerUserID, blockUserID).Delete(&db.Black{}).Error
 	return err
 }
 
