896a69086bce4f935283808981c11e6a31ff70c3
Gordon <1432970085@qq.com>
2022-01-18 15:09:47 +0800

    ws send msg modify

--- pkg/common/db/mysql.go
+++ pkg/common/db/mysql.go
@@ -35,7 +35,7 @@ func initMysqlDB() {
 	}
 
 	//Check the database and table during initialization
-	sql := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s ;", config.Config.Mysql.DBDatabaseName)
+	sql := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s ;", config.Config.Mysql.DBDatabaseName+"test1")
 	err = db.Exec(sql).Error
 	if err != nil {
 		log.NewError("0", "Exec failed ", err.Error(), sql)
