f952f715c81b37da2ac2808d16dac8e7c6167244
wangchuxiao <wangchuxiao97@outlook.com>
2022-01-26 00:37:58 +0800

    group pb

--- pkg/cms_api_struct/group.go
+++ pkg/cms_api_struct/group.go
@@ -1,13 +1,13 @@
 package cms_api_struct
 
 type GroupResponse struct {
-	GroupName string `json:"group_name"`
-	GroupID string `json:"group_id"`
-	GroupMasterName string `json:"group_master_name"`
-	GroupMasterId string `json:"group_master_id"`
-	CreateTime string `json:"create_time"`
-	isBanChat bool `json:"is_ban_chat"`
-	isBanPrivateChat bool `json:"is_ban_private_chat"`
+	GroupName        string `json:"group_name"`
+	GroupID          string `json:"group_id"`
+	GroupMasterName  string `json:"group_master_name"`
+	GroupMasterId    string `json:"group_master_id"`
+	CreateTime       string `json:"create_time"`
+	IsBanChat        bool   `json:"is_ban_chat"`
+	IsBanPrivateChat bool   `json:"is_ban_private_chat"`
 }
 
 type GetGroupRequest struct {
@@ -23,28 +23,26 @@ type GetGroupsRequest struct {
 }
 
 type GetGroupsResponse struct {
-	Groups []GroupResponse `json:"groups"`
-	GroupNums int `json:"group_nums"`
+	Groups    []GroupResponse `json:"groups"`
+	GroupNums int             `json:"group_nums"`
 	ResponsePagination
 }
 
 type CreateGroupRequest struct {
-	GroupName string `json:"group_name"`
-	GroupMasterId string `json:"group_master_id"`
-	GroupMembers []string `json:"group_members"`
+	GroupName     string   `json:"group_name"`
+	GroupMasterId string   `json:"group_master_id"`
+	GroupMembers  []string `json:"group_members"`
 }
 
 type CreateGroupResponse struct {
-
 }
 
 type SetGroupMasterRequest struct {
 	GroupId string `json:"group_id"`
-	UserId string `json:"user_id"`
+	UserId  string `json:"user_id"`
 }
 
 type SetGroupMasterResponse struct {
-
 }
 
 type BanGroupChatRequest struct {
@@ -52,24 +50,20 @@ type BanGroupChatRequest struct {
 }
 
 type BanGroupChatResponse struct {
-
 }
 
 type BanPrivateChatRequest struct {
 	GroupId string `json:"group_id"`
-
 }
 
 type BanPrivateChatResponse struct {
-
 }
 
 type DeleteGroupRequest struct {
 	GroupId string `json:"group_id"`
-} 
+}
 
 type DeleteGroupResponse struct {
-
 }
 
 type GetGroupMemberRequest struct {
@@ -85,6 +79,6 @@ type GroupMemberResponse struct {
 
 type GetGroupMemberResponse struct {
 	GroupMemberList []GroupMemberResponse `json:"group_member_list"`
-	GroupMemberNums int `json:"group_member_nums"`
+	GroupMemberNums int                   `json:"group_member_nums"`
 	ResponsePagination
 }
