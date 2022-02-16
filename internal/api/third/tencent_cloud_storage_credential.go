836b474cd80e5931b9a27511d9b9d197f62b3680
wenxu12345 <44203734@qq.com>
2022-02-16 18:30:24 +0800

    Refactoring code

--- internal/api/third/tencent_cloud_storage_credential.go
+++ internal/api/third/tencent_cloud_storage_credential.go
@@ -6,10 +6,11 @@ import (
 	"Open_IM/pkg/common/constant"
 	"Open_IM/pkg/common/log"
 	"Open_IM/pkg/common/token_verify"
-	"github.com/gin-gonic/gin"
-	sts "github.com/tencentyun/qcloud-cos-sts-sdk/go"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	sts "github.com/tencentyun/qcloud-cos-sts-sdk/go"
 )
 
 func TencentCloudStorageCredential(c *gin.Context) {
