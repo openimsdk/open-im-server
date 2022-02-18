29819e3e7f9cab9e9816a095969c0ada73e63781
wangchuxiao <wangchuxiao97@outlook.com>
2022-02-18 17:06:27 +0800

    mini cr

---
+++ internal/api/third/minio_storage_credential.go
@@ -0,0 +1,29 @@
+package apiThird
+
+import (
+	apiStruct "Open_IM/pkg/base_info"
+	"Open_IM/pkg/common/constant"
+	http "Open_IM/pkg/common/http"
+	"Open_IM/pkg/common/log"
+	"Open_IM/pkg/common/token_verify"
+	"github.com/gin-gonic/gin"
+	_ cr "github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func MinioStorageCredential(c *gin.Context) {
+	var (
+		req apiStruct.MinioStorageCredentialReq
+		resp apiStruct.MiniostorageCredentialResp
+	)
+	ok, _ := token_verify.GetUserIDFromToken(c.Request.Header.Get("token"))
+	if !ok {
+		log.NewError("", "GetUserIDFromToken false ", c.Request.Header.Get("token"))
+		http.RespHttp200(c, constant.ErrAccess, nil)
+		return
+	}
+	//var stsOpts cr.STSAssumeRoleOptions
+	//stsOpts.AccessKey = minioUsername
+	//stsOpts.SecretKey = minioPassword
+	log.NewInfo("0", req, resp)
+	http.RespHttp200(c, constant.OK, nil)
+}
