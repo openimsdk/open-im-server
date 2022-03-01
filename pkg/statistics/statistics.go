c4ec54f6e440b95a09c47e8a804b5fcbc613fe9e
skiffer-git <44203734@qq.com>
2022-03-01 15:22:02 +0800

    Statistics

--- pkg/statistics/statistics.go
+++ pkg/statistics/statistics.go
@@ -21,7 +21,7 @@ func (s *Statistics) output() {
 		select {
 		case <-t.C:
 		}
-		log.NewWarn("", "system stat ", s.ModuleName, s.PrintArgs, *s.Count-sum, "total:", *s.Count)
+		log.NewWarn("", " system stat ", s.ModuleName, s.PrintArgs, *s.Count-sum, "total:", *s.Count)
 	}
 }
 
