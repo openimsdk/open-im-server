7e9234e71262332b789c78a227681eb613983fce
Gordon <1432970085@qq.com>
2022-03-04 17:13:23 +0800

    log

--- pkg/common/log/file_line_hk.go
+++ pkg/common/log/file_line_hk.go
@@ -31,9 +31,9 @@ func (f *fileHook) Levels() []logrus.Level {
 func (f *fileHook) Fire(entry *logrus.Entry) error {
 	var s string
 	_, b, c, _ := runtime.Caller(8)
-	i := strings.LastIndex(b, "/")
-	if i != -1 {
-		s = b[i+1:len(b)] + ":" + utils.IntToString(c)
+	i := strings.SplitAfter(b, "/")
+	if len(i) > 3 {
+		s = i[len(i)-3] + i[len(i)-2] + i[len(i)-1] + ":" + utils.IntToString(c)
 	}
 	entry.Data["FilePath"] = s
 	return nil
