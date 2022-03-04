1bcd2d453128181e4b1db30210c76975d4bd0e64
Gordon <1432970085@qq.com>
2022-03-04 17:26:50 +0800

    log

--- pkg/common/log/file_line_hk.go
+++ pkg/common/log/file_line_hk.go
@@ -30,7 +30,7 @@ func (f *fileHook) Levels() []logrus.Level {
 
 func (f *fileHook) Fire(entry *logrus.Entry) error {
 	var s string
-	_, b, c, _ := runtime.Caller(8)
+	_, b, c, _ := runtime.Caller(10)
 	i := strings.SplitAfter(b, "/")
 	if len(i) > 3 {
 		s = i[len(i)-3] + i[len(i)-2] + i[len(i)-1] + ":" + utils.IntToString(c)
