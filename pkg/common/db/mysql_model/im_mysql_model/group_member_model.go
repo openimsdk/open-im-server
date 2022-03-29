bc260fffde073a7875328707318fe631a4071a0f
Gordon <1432970085@qq.com>
2022-03-29 17:15:51 +0800

    group add

--- pkg/common/db/mysql_model/im_mysql_model/group_member_model.go
+++ pkg/common/db/mysql_model/im_mysql_model/group_member_model.go
@@ -31,7 +31,7 @@ func InsertIntoGroupMember(toInsertInfo db.GroupMember) error {
 		toInsertInfo.RoleLevel = constant.GroupOrdinaryUsers
 	}
 	if toInsertInfo.MuteEndTime.Unix() == 0 {
-		toInsertInfo.MuteEndTime = time.Unix(1, 0)
+		toInsertInfo.MuteEndTime = time.Unix(1, 1)
 	}
 	err = dbConn.Table("group_members").Create(toInsertInfo).Error
 	if err != nil {
