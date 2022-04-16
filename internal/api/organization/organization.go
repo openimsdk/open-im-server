4a7f406edbbc68ff6388db73f363ed37b08c659a
skiffer-git <44203734@qq.com>
2022-04-16 21:17:32 +0800

    organization

--- internal/api/organization/organization.go
+++ internal/api/organization/organization.go
@@ -33,7 +33,7 @@ func CreateDepartment(c *gin.Context) {
 		return
 	}
 
-	log.NewInfo(req.OperationID, utils.GetSelfFuncName(), "api args ", req.String())
+	log.NewInfo(req.OperationID, utils.GetSelfFuncName(), "api args ", req.String(), "params", params)
 	etcdConn := getcdv3.GetConn(config.Config.Etcd.EtcdSchema, strings.Join(config.Config.Etcd.EtcdAddr, ","), config.Config.RpcRegisterName.OpenImOrganizationName)
 	client := rpc.NewOrganizationClient(etcdConn)
 	RpcResp, err := client.CreateDepartment(context.Background(), req)
