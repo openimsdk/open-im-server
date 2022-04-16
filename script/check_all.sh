bef4f0c4e25c1cdc97f65f4a3beb22c8cd1a79fd
skiffer-git <44203734@qq.com>
2022-04-16 20:25:01 +0800

    organization

--- script/check_all.sh
+++ script/check_all.sh
@@ -20,6 +20,7 @@ service_port_name=(
   openImMessageCmsPort
   openImStatisticsPort
   openImOfficePort
+  openImOrganizationPort
 )
 switch=$(cat $config_path | grep demoswitch |awk -F '[:]' '{print $NF}')
 for i in ${service_port_name[*]}; do
