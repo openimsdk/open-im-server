d3aa3dbf41421d81605d60d2b86cee1250e76315
skiffer-git <44203734@qq.com>
2022-04-18 21:16:23 +0800

    add operationID when ws connecting

--- pkg/common/token_verify/jwt_token.go
+++ pkg/common/token_verify/jwt_token.go
@@ -218,13 +218,13 @@ func VerifyToken(token, uid string) (bool, error) {
 func WsVerifyToken(token, uid string, platformID string) (bool, error, string) {
 	claims, err := ParseToken(token, "")
 	if err != nil {
-		return false, err, "parse token err"
+		return false, utils.Wrap(err, ""), "parse token err"
 	}
 	if claims.UID != uid {
-		return false, &constant.ErrTokenUnknown, "uid is not same to token uid"
+		return false, utils.Wrap(&constant.ErrTokenUnknown, ""), "uid is not same to token uid"
 	}
 	if claims.Platform != constant.PlatformIDToName(utils.StringToInt32(platformID)) {
-		return false, &constant.ErrTokenUnknown, "platform is not same to token platform"
+		return false, utils.Wrap(&constant.ErrTokenUnknown, ""), "platform is not same to token platform"
 	}
 	log.NewDebug("", claims.UID, claims.Platform)
 	return true, nil, ""
