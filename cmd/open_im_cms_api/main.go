4eb96617307f8019648b492c5f0ae307c738ae0b
wangchuxiao <wangchuxiao97@outlook.com>
2022-04-24 11:50:36 +0800

    fix docker-compose

--- cmd/open_im_cms_api/main.go
+++ cmd/open_im_cms_api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"Open_IM/internal/cms_api"
 	"Open_IM/pkg/utils"
+	"flag"
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +14,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := cms_api.NewGinRouter()
 	router.Use(utils.CorsHandler())
-	fmt.Println("start cms api server, port: ", 8000)
-	router.Run(":" + "8000")
+	ginPort := flag.Int("port", 8000, "get ginServerPort from cmd,default 10000 as port")
+	fmt.Println("start cms api server, port: ", ginPort)
+	router.Run(":" + strconv.Itoa(*ginPort))
 }
