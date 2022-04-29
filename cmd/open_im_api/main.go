8554f1e22803000bdbeae2c71f8016d45f084a32
wangchuxiao <wangchuxiao97@outlook.com>
2022-04-29 17:59:42 +0800

    cache & workMoments

--- cmd/open_im_api/main.go
+++ cmd/open_im_api/main.go
@@ -30,7 +30,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	f, _ := os.Create("../logs/api.log")
 	gin.DefaultWriter = io.MultiWriter(f)
-
+	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
 	r.Use(utils.CorsHandler())
 
