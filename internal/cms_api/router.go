fab52b6a5695603b3e806eed557da9d8a465837a
wangchuxiao <wangchuxiao97@outlook.com>
2022-05-10 16:23:56 +0800

    update app

--- internal/cms_api/router.go
+++ internal/cms_api/router.go
@@ -22,7 +22,7 @@ func NewGinRouter() *gin.Engine {
 		adminRouterGroup.POST("/login", admin.AdminLogin)
 		adminRouterGroup.POST("/upload_update_app", admin.UploadUpdateApp)
 		adminRouterGroup.POST("/get_download_url", admin.GetDownloadURL)
-		adminRouterGroup.POST("/test", admin.UploadUpdateApp)
+		adminRouterGroup.POST("/test", admin.MinioUploadFile)
 	}
 	r2 := router.Group("")
 	r2.Use(middleware.JWTAuth())
