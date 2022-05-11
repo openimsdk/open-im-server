78a78ab22a68890b2492e06b61b36b69a096772c
wangchuxiao <wangchuxiao97@outlook.com>
2022-05-11 10:27:00 +0800

    update app

--- pkg/common/db/mysql.go
+++ pkg/common/db/mysql.go
@@ -59,7 +59,7 @@ func initMysqlDB() {
 		&GroupMember{},
 		&GroupRequest{},
 		&User{},
-		&Black{}, &ChatLog{}, &Register{}, &Conversation{})
+		&Black{}, &ChatLog{}, &Register{}, &Conversation{}, &AppVersion{})
 	db.Set("gorm:table_options", "CHARSET=utf8")
 	db.Set("gorm:table_options", "collation=utf8_unicode_ci")
 
