06726758d218d96330a559378b2b0b15e8b0f700
Gordon <1432970085@qq.com>
2022-05-20 13:33:38 +0800

    concurrent consumption of messages

--- internal/msg_transfer/logic/offline_history_msg_handler.go
+++ internal/msg_transfer/logic/offline_history_msg_handler.go
@@ -15,22 +15,28 @@ import (
 
 type OfflineHistoryConsumerHandler struct {
 	msgHandle            map[string]fcb
+	historyConsumerGroup *kfk.MConsumerGroup
 	cmdCh                chan Cmd2Value
 	msgCh                chan Cmd2Value
-	historyConsumerGroup *kfk.MConsumerGroup
+	UserAggregationMsgs  map[string][]*pbMsg.MsgDataToMQ
+	chArrays             [ChannelNum]chan Cmd2Value
+	msgDistributionCh    chan Cmd2Value
 }
 
 func (mc *OfflineHistoryConsumerHandler) Init(cmdCh chan Cmd2Value) {
 	mc.msgHandle = make(map[string]fcb)
+	mc.UserAggregationMsgs = make(map[string][]*pbMsg.MsgDataToMQ, ChannelNum)
+	mc.msgDistributionCh = make(chan Cmd2Value) //no buffer channel
+	go mc.MessagesDistributionHandle()
 	mc.cmdCh = cmdCh
 	mc.msgCh = make(chan Cmd2Value, 1000)
 	if config.Config.ReliableStorage {
-		mc.msgHandle[config.Config.Kafka.Ws2mschatOffline.Topic] = mc.handleChatWs2Mongo
+		mc.msgHandle[config.Config.Kafka.Ws2mschat.Topic] = mc.handleChatWs2Mongo
 	} else {
-		mc.msgHandle[config.Config.Kafka.Ws2mschatOffline.Topic] = mc.handleChatWs2MongoLowReliability
-		for i := 0; i < 10; i++ {
-			go mc.Run()
-
+		mc.msgHandle[config.Config.Kafka.Ws2mschat.Topic] = mc.handleChatWs2MongoLowReliability
+		for i := 0; i < ChannelNum; i++ {
+			mc.chArrays[i] = make(chan Cmd2Value, 1000)
+			go mc.Run(i)
 		}
 	}
 	mc.historyConsumerGroup = kfk.NewMConsumerGroup(&kfk.MConsumerGroupConfig{KafkaVersion: sarama.V0_10_2_0,
@@ -38,31 +44,91 @@ func (mc *OfflineHistoryConsumerHandler) Init(cmdCh chan Cmd2Value) {
 		config.Config.Kafka.Ws2mschatOffline.Addr, config.Config.Kafka.ConsumerGroupID.MsgToMongoOffline)
 
 }
-func (mc *OfflineHistoryConsumerHandler) Run() {
+func (och *OfflineHistoryConsumerHandler) Run(channelID int) {
 	for {
 		select {
-		case cmd := <-mc.msgCh:
+		case cmd := <-och.chArrays[channelID]:
 			switch cmd.Cmd {
-			case Msg:
+			case UserMessages:
 				msgChannelValue := cmd.Value.(MsgChannelValue)
-				msg := msgChannelValue.msg
-				log.Debug(msg.OperationID, "msg arrived channel", msg.String())
-				isSenderSync := utils.GetSwitchFromOptions(msg.MsgData.Options, constant.IsSenderSync)
-				err := saveUserChat(msgChannelValue.userID, &msg)
+				msgList := msgChannelValue.msgList
+				storageMsgList := make([]*pbMsg.MsgDataToMQ, 80)
+				pushMsgList := make([]*pbMsg.MsgDataToMQ, 80)
+				latestMsgOperationID := msgList[len(msgList)-1].OperationID
+				log.Debug(latestMsgOperationID, "msg arrived channel", "channel id", channelID, msgList)
+				for _, v := range msgList {
+					isHistory := utils.GetSwitchFromOptions(v.MsgData.Options, constant.IsHistory)
+					isSenderSync := utils.GetSwitchFromOptions(v.MsgData.Options, constant.IsSenderSync)
+					if isHistory {
+						storageMsgList = append(storageMsgList, v)
+					}
+					if !(!isSenderSync && msgChannelValue.userID == v.MsgData.SendID) {
+						pushMsgList = append(pushMsgList, v)
+					}
+				}
+
+				//switch msgChannelValue.msg.MsgData.SessionType {
+				//case constant.SingleChatType:
+				//case constant.GroupChatType:
+				//case constant.NotificationChatType:
+				//default:
+				//	log.NewError(msgFromMQ.OperationID, "SessionType error", msgFromMQ.String())
+				//	return
+				//}
+
+				err := saveUserChatList(msgChannelValue.userID, storageMsgList, latestMsgOperationID)
 				if err != nil {
-					singleMsgFailedCount++
-					log.NewError(msg.OperationID, "single data insert to mongo err", err.Error(), msg.String())
+					singleMsgFailedCount += uint64(len(storageMsgList))
+					log.NewError(latestMsgOperationID, "single data insert to mongo err", err.Error(), storageMsgList)
 				} else {
 					singleMsgSuccessCountMutex.Lock()
-					singleMsgSuccessCount++
+					singleMsgSuccessCount += uint64(len(storageMsgList))
 					singleMsgSuccessCountMutex.Unlock()
-					if !(!isSenderSync && msgChannelValue.userID == msg.MsgData.SendID) {
-						go sendMessageToPush(&msg, msgChannelValue.userID)
+					for _, v := range pushMsgList {
+						sendMessageToPush(v, msgChannelValue.userID)
+					}
+
+				}
+			}
+		}
+	}
+}
+func (och *OfflineHistoryConsumerHandler) MessagesDistributionHandle() {
+	for {
+		select {
+		case cmd := <-och.msgDistributionCh:
+			switch cmd.Cmd {
+			case ConsumerMsgs:
+				consumerMessages := cmd.Value.([]*sarama.ConsumerMessage)
+				//Aggregation map[userid]message list
+				for i := 0; i < len(consumerMessages); i++ {
+					msgFromMQ := pbMsg.MsgDataToMQ{}
+					err := proto.Unmarshal(consumerMessages[i].Value, &msgFromMQ)
+					if err != nil {
+						log.Error("msg_transfer Unmarshal msg err", "", "msg", string(consumerMessages[i].Value), "err", err.Error())
+						return
+					}
+					if oldM, ok := och.UserAggregationMsgs[string(consumerMessages[i].Key)]; ok {
+						oldM = append(oldM, &msgFromMQ)
+						och.UserAggregationMsgs[string(consumerMessages[i].Key)] = oldM
+					} else {
+						m := make([]*pbMsg.MsgDataToMQ, 100)
+						m = append(m, &msgFromMQ)
+						och.UserAggregationMsgs[string(consumerMessages[i].Key)] = m
+					}
+				}
+				for userID, v := range och.UserAggregationMsgs {
+					if len(v) >= 0 {
+						channelID := getHashCode(userID) % ChannelNum
+						go func(cID uint32, userID string, messages []*pbMsg.MsgDataToMQ) {
+							och.chArrays[cID] <- Cmd2Value{Cmd: UserMessages, Value: MsgChannelValue{userID: userID, msgList: messages}}
+						}(channelID, userID, v)
 					}
 				}
 			}
 		}
 	}
+
 }
 func (mc *OfflineHistoryConsumerHandler) handleChatWs2Mongo(cMsg *sarama.ConsumerMessage, msgKey string, sess sarama.ConsumerGroupSession) {
 	msg := cMsg.Value
@@ -157,7 +223,7 @@ func (mc *OfflineHistoryConsumerHandler) handleChatWs2MongoLowReliability(cMsg *
 		sess.MarkMessage(cMsg, "")
 		msgFromMQ.MsgData.Seq = uint32(seq)
 		log.Debug(operationID, "send ch msg is ", msgFromMQ.String())
-		mc.msgCh <- Cmd2Value{Cmd: Msg, Value: MsgChannelValue{msgKey, msgFromMQ}}
+		//mc.msgCh <- Cmd2Value{Cmd: Msg, Value: MsgChannelValue{msgKey, msgFromMQ}}
 		//err := saveUserChat(msgKey, &msgFromMQ)
 		//if err != nil {
 		//	singleMsgFailedCount++
@@ -177,27 +243,56 @@ func (mc *OfflineHistoryConsumerHandler) handleChatWs2MongoLowReliability(cMsg *
 
 func (OfflineHistoryConsumerHandler) Setup(_ sarama.ConsumerGroupSession) error   { return nil }
 func (OfflineHistoryConsumerHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
-func (mc *OfflineHistoryConsumerHandler) ConsumeClaim(sess sarama.ConsumerGroupSession,
-	claim sarama.ConsumerGroupClaim) error { // a instance in the consumer group
-	//log.NewDebug("", "offline new session msg come", claim.HighWaterMarkOffset(), claim.Topic(), claim.Partition())
-	//for msg := range claim.Messages() {
-	//	log.NewDebug("", "kafka get info to delay mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "offline")
-	//	//mc.msgHandle[msg.Topic](msg.Value, string(msg.Key))
-	//}
-	for msg := range claim.Messages() {
-		if GetOnlineTopicStatus() == OnlineTopicVacancy {
-			log.NewDebug("", "vacancy offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
-			mc.msgHandle[msg.Topic](msg, string(msg.Key), sess)
-		} else {
-			select {
-			case <-mc.cmdCh:
-				log.NewDebug("", "cmd offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
-			case <-time.After(time.Millisecond * time.Duration(100)):
-				log.NewDebug("", "timeout offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
-			}
-			mc.msgHandle[msg.Topic](msg, string(msg.Key), sess)
-		}
-	}
 
+//func (mc *OfflineHistoryConsumerHandler) ConsumeClaim(sess sarama.ConsumerGroupSession,
+//	claim sarama.ConsumerGroupClaim) error { // a instance in the consumer group
+//	//log.NewDebug("", "offline new session msg come", claim.HighWaterMarkOffset(), claim.Topic(), claim.Partition())
+//	//for msg := range claim.Messages() {
+//	//	log.NewDebug("", "kafka get info to delay mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "offline")
+//	//	//mc.msgHandle[msg.Topic](msg.Value, string(msg.Key))
+//	//}
+//	for msg := range claim.Messages() {
+//		if GetOnlineTopicStatus() == OnlineTopicVacancy {
+//			log.NewDebug("", "vacancy offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
+//			mc.msgHandle[msg.Topic](msg, string(msg.Key), sess)
+//		} else {
+//			select {
+//			case <-mc.cmdCh:
+//				log.NewDebug("", "cmd offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
+//			case <-time.After(time.Millisecond * time.Duration(100)):
+//				log.NewDebug("", "timeout offline kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "msg", string(msg.Value))
+//			}
+//			mc.msgHandle[msg.Topic](msg, string(msg.Key), sess)
+//		}
+//	}
+//
+//	return nil
+//}
+func (och *OfflineHistoryConsumerHandler) ConsumeClaim(sess sarama.ConsumerGroupSession,
+	claim sarama.ConsumerGroupClaim) error { // a instance in the consumer group
+	log.NewDebug("", "online new session msg come", claim.HighWaterMarkOffset(), claim.Topic(), claim.Partition())
+	cMsg := make([]*sarama.ConsumerMessage, 500)
+	t := time.NewTicker(time.Duration(500) * time.Millisecond)
+	for msg := range claim.Messages() {
+		//och.TriggerCmd(OnlineTopicBusy)
+		cMsg = append(cMsg, msg)
+		select {
+		case <-t.C:
+			if len(cMsg) >= 0 {
+				och.msgDistributionCh <- Cmd2Value{Cmd: ConsumerMsgs, Value: cMsg}
+				sess.MarkMessage(msg, "")
+				cMsg = cMsg[0:0]
+			}
+		default:
+			if len(cMsg) >= 500 {
+				och.msgDistributionCh <- Cmd2Value{Cmd: ConsumerMsgs, Value: cMsg}
+				sess.MarkMessage(msg, "")
+				cMsg = cMsg[0:0]
+			}
+
+		}
+		log.NewDebug("", "online kafka get info to mongo", "msgTopic", msg.Topic, "msgPartition", msg.Partition, "online", msg.Offset, claim.HighWaterMarkOffset())
+
+	}
 	return nil
 }
