1bfefbd08336e8fa4c12458c07e5d1a392fd2de5
wenxu12345 <44203734@qq.com>
2022-05-27 20:29:39 +0800

    super group

--- pkg/proto/auto_proto.sh
+++ pkg/proto/auto_proto.sh
@@ -4,10 +4,14 @@ source ./proto_dir.cfg
 
 for ((i = 0; i < ${#all_proto[*]}; i++)); do
   proto=${all_proto[$i]}
-  protoc -I ../../../ -I ./ --go_out=plugins=grpc:. $proto
+
+  protoc -I ../../../  -I ./ --go_out=plugins=grpc:. $proto
   s=`echo $proto | sed 's/ //g'`
   v=${s//proto/pb.go}
   protoc-go-inject-tag -input=./$v
   echo "protoc --go_out=plugins=grpc:." $proto
 done
 echo "proto file generate success..."
+
+find ./ -type f -path "*.pb.go"|xargs sed -i 's/\".\/sdk_ws\"/\"Open_IM\/pkg\/proto\/sdk_ws\"/g'
+
