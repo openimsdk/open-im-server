da1df91f49ed4951f60b01f2df64a98929b2286d
wenxu12345 <44203734@qq.com>
2022-06-01 15:54:40 +0800

    Clear msg

--- internal/rpc/msg/chat.go
+++ internal/rpc/msg/chat.go
@@ -25,7 +25,7 @@ func (rpc *rpcChat) ClearMsg(_ context.Context, req *pbChat.ClearMsgReq) (*pbCha
 		return &pbChat.ClearMsgResp{ErrCode: constant.ErrDB.ErrCode, ErrMsg: errMsg}, nil
 	}
 	log.Debug(req.OperationID, "CleanUpUserMsgFromMongo args", req.UserID)
-	err = db.DB.CleanUpUserMsgFromMongo(req.UserID)
+	err = db.DB.CleanUpUserMsgFromMongo(req.UserID, req.OperationID)
 	if err != nil {
 		errMsg := "CleanUpUserMsgFromMongo failed " + err.Error() + req.OperationID + req.UserID
 		log.Error(req.OperationID, errMsg)
