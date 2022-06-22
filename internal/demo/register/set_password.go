c4ea10d0af46448f42f71232878840855f400537
wangchuxiao <wangchuxiao97@outlook.com>
2022-06-22 20:23:04 +0800

    fix bug

--- internal/demo/register/set_password.go
+++ internal/demo/register/set_password.go
@@ -87,7 +87,7 @@ func SetPassword(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"errCode": constant.RegisterFailed, "errMsg": "register failed: " + openIMRegisterResp.ErrMsg})
 		return
 	}
-	log.Info(params.OperationID, "begin store mysql", account, params.Password, params.FaceURL, params.Nickname)
+	log.Info(params.OperationID, "begin store mysql", account, params.Password, "info", params.FaceURL, params.Nickname)
 	err = im_mysql_model.SetPassword(account, params.Password, params.Ex, userID)
 	if err != nil {
 		log.NewError(params.OperationID, "set phone number password error", account, "err", err.Error())
