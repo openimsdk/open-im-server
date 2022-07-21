eec8eb4766304d8be5faed87b9bc0092bcb11c53
wangchuxiao <wangchuxiao97@outlook.com>
2022-07-21 11:20:18 +0800

    dtm

--- pkg/common/db/mysql.go
+++ pkg/common/db/mysql.go
@@ -57,7 +57,7 @@ func initMysqlDB() {
 		Writer{},
 		logger.Config{
 			SlowThreshold:             200 * time.Millisecond, // Slow SQL threshold
-			LogLevel:                  logger.Silent,          // Log level
+			LogLevel:                  logger.Error,           // Log level
 			IgnoreRecordNotFoundError: true,                   // Ignore ErrRecordNotFound error for logger
 			Colorful:                  true,                   // Disable color
 		},
