f3ed48e0ac73d3261167cf1b4e206b1d9af29299
Gordon <1432970085@qq.com>
2022-07-22 19:20:57 +0800

    seq add

--- internal/rpc/msg/pull_message.go
+++ internal/rpc/msg/pull_message.go
@@ -40,6 +40,7 @@ func (rpc *rpcChat) GetMaxAndMinSeq(_ context.Context, in *open_im_sdk.GetMaxAnd
 		x.MinSeq = uint32(minSeq)
 		m[groupID] = x
 	}
+	resp.GroupMaxAndMinSeq = m
 	return resp, nil
 }
 func (rpc *rpcChat) PullMessageBySeqList(_ context.Context, in *open_im_sdk.PullMessageBySeqListReq) (*open_im_sdk.PullMessageBySeqListResp, error) {
