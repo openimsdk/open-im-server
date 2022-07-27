c458d59da50d7a77ebf838bc188f67b65be0d52b
wenxu12345 <44203734@qq.com>
2022-07-27 11:49:00 +0800

    debug

--- internal/msg_gateway/gate/open_im_media/room.go
+++ internal/msg_gateway/gate/open_im_media/room.go
@@ -5,8 +5,6 @@ const (
 	Address = "127.0.0.1:11300"
 )
 
-//var roomClient *lksdk.RoomServiceClient
-
 type Media struct {
 }
 
@@ -14,45 +12,11 @@ func NewMedia() *Media {
 	return &Media{}
 }
 
-//func (m *Media) GetJoinToken(room, identity string, operationID string, data *open_im_sdk.ParticipantMetaData) (string, string, error) {
-//	var newData pbRtc.ParticipantMetaData
-//	copier.Copy(&newData, data)
-//	conn, err := grpc.Dial(Address, grpc.WithInsecure())
-//	if err != nil {
-//		return "", "", err
-//	}
-//	defer conn.Close()
-//	c := pbRtc.NewRtcServiceClient(conn)
-//	req := &pbRtc.GetJoinTokenReq{Room: room, OperationID: operationID, Identity: identity, MetaData: &newData}
-//	resp, err := c.GetJoinToken(context.Background(), req)
-//	if err != nil {
-//		return "", "", err
-//	}
-//	if resp.CommonResp.ErrCode != 0 {
-//		return "", "", errors.New(resp.CommonResp.ErrMsg)
-//	}
-//	return resp.Jwt, resp.LiveURL, nil
-//	//at := auth.NewAccessToken(m.ApiKey, m.ApiSecret)
-//	//grant := &auth.VideoGrant{
-//	//	RoomJoin: true,
-//	//	Room:     room,
-//	//}
-//	//at.AddGrant(grant).
-//	//	SetIdentity(identity).
-//	//	SetValidFor(time.Hour)
-//	//
-//	//return at.ToJWT()
-//}
-
 func init() {
-	//roomClient = lksdk.NewRoomServiceClient(MediaAddress, ApiKey, ApiSecret)
+
 }
 
 func (m *Media) CreateRoom(roomName string) (error, error) {
 	return nil, nil
-	//return roomClient.CreateRoom(context.Background(), &livekit.CreateRoomRequest{
-	//	Name:         roomName,
-	//	EmptyTimeout: 60 * 3,
-	//})
 
 }
