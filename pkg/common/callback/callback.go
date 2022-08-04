0117e96d5b20ce82fb03d9c567e5a79baef52383
Gordon <1432970085@qq.com>
2022-08-04 16:27:39 +0800

    callback msg content

--- pkg/common/callback/callback.go
+++ pkg/common/callback/callback.go
@@ -3,7 +3,6 @@ package callback
 import (
 	"Open_IM/pkg/common/constant"
 	server_api_params "Open_IM/pkg/proto/sdk_ws"
-	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 )
 
@@ -11,12 +10,12 @@ func GetContent(msg *server_api_params.MsgData) string {
 	if msg.ContentType >= constant.NotificationBegin && msg.ContentType <= constant.NotificationEnd {
 		var tips server_api_params.TipsComm
 		_ = proto.Unmarshal(msg.Content, &tips)
-		marshaler := jsonpb.Marshaler{
-			OrigName:     true,
-			EnumsAsInts:  false,
-			EmitDefaults: false,
-		}
-		content, _ := marshaler.MarshalToString(&tips)
+		//marshaler := jsonpb.Marshaler{
+		//	OrigName:     true,
+		//	EnumsAsInts:  false,
+		//	EmitDefaults: false,
+		//}
+		content := tips.JsonDetail
 		return content
 	} else {
 		return string(msg.Content)
