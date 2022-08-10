d6200180d9c9a9a299e1567f1bdf350b8d962ea6
wangchuxiao <wangchuxiao97@outlook.com>
2022-08-10 20:34:18 +0800

    fix delete

--- script/start_all.sh
+++ script/start_all.sh
@@ -10,6 +10,7 @@ need_to_start_server_shell=(
   msg_transfer_start.sh
   sdk_svr_start.sh
   demo_svr_start.sh
+  start_cron.sh
 )
 time=`date +"%Y-%m-%d %H:%M:%S"`
 echo "==========================================================">>../logs/openIM.log 2>&1 &
