1da5ad88302ec268b21dd2545ead48b365d4f6a5
wangchuxiao <wangchuxiao97@outlook.com>
2022-08-31 01:29:29 +0800

    add

--- pkg/common/db/model_struct.go
+++ pkg/common/db/model_struct.go
@@ -227,17 +227,17 @@ type Black struct {
 type ChatLog struct {
 	ServerMsgID      string    `gorm:"column:server_msg_id;primary_key;type:char(64)" json:"serverMsgID"`
 	ClientMsgID      string    `gorm:"column:client_msg_id;type:char(64)" json:"clientMsgID"`
-	SendID           string    `gorm:"column:send_id;type:char(64);index:send_id" json:"sendID"`
-	RecvID           string    `gorm:"column:recv_id;type:char(64);index:recv_id" json:"recvID"`
+	SendID           string    `gorm:"column:send_id;type:char(64);index:send_id,priority:2" json:"sendID"`
+	RecvID           string    `gorm:"column:recv_id;type:char(64);index:recv_id,priority:2" json:"recvID"`
 	SenderPlatformID int32     `gorm:"column:sender_platform_id" json:"senderPlatformID"`
 	SenderNickname   string    `gorm:"column:sender_nick_name;type:varchar(255)" json:"senderNickname"`
-	SenderFaceURL    string    `gorm:"column:sender_face_url;type:varchar(255)" json:"senderFaceURL"`
-	SessionType      int32     `gorm:"column:session_type;index:session_type" json:"sessionType"`
+	SenderFaceURL    string    `gorm:"column:sender_face_url;type:varchar(255);index:content_type,priority:1" json:"senderFaceURL"`
+	SessionType      int32     `gorm:"column:session_type;index:session_type,priority:2" json:"sessionType"`
 	MsgFrom          int32     `gorm:"column:msg_from" json:"msgFrom"`
-	ContentType      int32     `gorm:"column:content_type;index:content_type" json:"contentType"`
+	ContentType      int32     `gorm:"column:content_type;index:content_type,priority:2" json:"contentType"`
 	Content          string    `gorm:"column:content;type:varchar(3000)" json:"content"`
 	Status           int32     `gorm:"column:status" json:"status"`
-	SendTime         time.Time `gorm:"column:send_time;index:sendTime" json:"sendTime"`
+	SendTime         time.Time `gorm:"column:send_time;index:sendTime;index:content_type,priority:1;index:session_type,priority:1;index:recv_id,priority:1;index:send_id,priority:1" json:"sendTime"`
 	CreateTime       time.Time `gorm:"column:create_time" json:"createTime"`
 	Ex               string    `gorm:"column:ex;type:varchar(1024)" json:"ex"`
 }
