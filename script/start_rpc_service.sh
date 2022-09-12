7a9b45d25765fda9585cf1591bb974f28d059a16
wangchuxiao <wangchuxiao97@outlook.com>
2022-09-12 22:55:30 +0800

    add prome

--- script/start_rpc_service.sh
+++ script/start_rpc_service.sh
@@ -84,7 +84,7 @@ for ((i = 0; i < ${#service_filename[*]}; i++)); do
   for j in ${service_ports}; do
     #Start the service in the background
     #    ./${service_filename[$i]} -port $j &
-    echo $i ${prome_ports}
+    echo $i ${prome_ports} ${prome_ports[$i]}
     cmd="./${service_filename[$i]} -port $j -prometheus_port ${prome_ports[$i]}"
     if [ $i -eq 0 -o $i -eq 1 ]; then
       cmd="./${service_filename[$i]} -port $j"
