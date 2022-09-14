88af048e907c38438fb048573a0e619cf6527929
wangchuxiao <wangchuxiao97@outlook.com>
2022-09-14 12:19:17 +0800

    add log

--- pkg/common/db/mysql.go
+++ pkg/common/db/mysql.go
@@ -23,6 +23,7 @@ func (w Writer) Printf(format string, args ...interface{}) {
 }
 
 func initMysqlDB() {
+	fmt.Println("init mysqlDB start")
 	//When there is no open IM database, connect to the mysql built-in database to create openIM database
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
 		config.Config.Mysql.DBUserName, config.Config.Mysql.DBPassword, config.Config.Mysql.DBAddress[0], "mysql")
