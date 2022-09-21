1c5aae2301f127714913c00e1a0d6d0a50d854bb
skiffer-git <44203734@qq.com>
2022-09-21 09:09:00 +0800

    update proto

--- pkg/proto/auto_proto.sh
+++ pkg/proto/auto_proto.sh
@@ -10,6 +10,19 @@ for ((i = 0; i < ${#all_proto[*]}; i++)); do
 done
 echo "proto file generate success"
 
+
+j=0
+for file in $(find ./Open_IM -name   "*.go"); do # Not recommended, will break on whitespace
+    filelist[j]=$file
+    j=`expr $j + 1`
+done
+
+
+for ((i = 0; i < ${#filelist[*]}; i++)); do
+  proto=${filelist[$i]}
+  cp $proto  ${proto#*./Open_IM/pkg/proto/}
+done
+rm Open_IM -rf
 #find ./ -type f -path "*.pb.go"|xargs sed -i 's/\".\/sdk_ws\"/\"Open_IM\/pkg\/proto\/sdk_ws\"/g'
 
 
