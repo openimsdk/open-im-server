051f73013cddacfd82804b2b0cca3e4e4e6acf56
wangchuxiao <wangchuxiao97@outlook.com>
2022-11-14 19:36:49 +0800

    etcd conf

--- script/init_pwd.sh
+++ script/init_pwd.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
-
 source ../.env
 echo "your user is:$USER"
 echo "your password is:$PASSWORD"
-echo "your minio endPoint:$MINIO_ENDPOINT"
+echo "your minio endPoint is:$MINIO_ENDPOINT"
 
-sed -i "/^\([[:space:]]*dbMysqlUserName: *\).*/s//\1$USER/;/\([[:space:]]*dbUserName: *\).*/s//\1 $USER/;/\([[:space:]]*accessKeyID: *\).*/s//\1$USER/;/\([[:space:]]*endpoint: *\).*/s//\1\"$MINIO_ENDPOINT\"/;" ../config/usualConfig.yaml
+sed -i "/^\([[:space:]]*dbMysqlUserName: *\).*/s//\1$USER/;/\([[:space:]]*dbUserName: *\).*/s//\1 $USER/;/\([[:space:]]*accessKeyID: *\).*/s//\1$USER/;/\([[:space:]]*endpoint: *\).*/s//\1\"abc\"/;" ../config/usualConfig.yaml
 sed -i "/^\([[:space:]]*dbMysqlPassword: *\).*/s//\1$PASSWORD/;/\([[:space:]]*dbPassword: *\).*/s//\1$PASSWORD/;/\([[:space:]]*dbPassWord: *\).*/s//\1$PASSWORD/;/\([[:space:]]*secretAccessKey: *\).*/s//\1$PASSWORD/;" ../config/usualConfig.yaml
+
+sed -i "/\([[:space:]]*endpoint: *\).*/s##\1$MINIO_ENDPOINT#;" ../config/usualConfig.yaml
