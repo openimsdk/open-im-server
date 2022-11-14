99e1c53a41098478bea19041e5d3a3f1fe27d07c
wangchuxiao <wangchuxiao97@outlook.com>
2022-11-14 15:41:24 +0800

    conf script

--- script/init_pwd.sh
+++ script/init_pwd.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 source ../.env
+echo "user:{$USER}"
+echo "password:{$PASSWORD}"
 
 nameList=(dbMysqlUserName, dbUserName, dbUserName, accessKeyID)
 pwdList=(dbMysqlPassword, dbPassword, dbPassWord, secretAccessKey)
 
 for i in ${nameList[*]}; do
+  echo {$i}
  sed -i 's/{$i}: [a-z]/{$i}: {$USER}/g' ../config/usualConfig.yaml
 done
 
 for i in ${pwdList[*]}; do
+  echo {$i}
  sed -i 's/{$i}: [a-z]/{$i}: {$PASSWORD}/g' ../config/usualConfig.yaml
 done
