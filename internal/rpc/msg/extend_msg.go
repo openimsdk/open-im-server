53cd4205e813db3b8d584887d5b67134d936f809
wangchuxiao <wangchuxiao97@outlook.com>
2022-12-09 10:45:51 +0800

    modify

--- internal/rpc/msg/extend_msg.go
+++ internal/rpc/msg/extend_msg.go
@@ -1,1 +1,22 @@
 package msg
+
+import (
+	"Open_IM/pkg/proto/msg"
+	"context"
+)
+
+func (rpc *rpcChat) SetMessageReactionExtensions(ctx context.Context, req *msg.ModifyMessageReactionExtensionsReq) (resp *msg.ModifyMessageReactionExtensionsResp, err error) {
+	return
+}
+
+func (rpc *rpcChat) GetMessageListReactionExtensions(ctx context.Context, req *msg.OperateMessageListReactionExtensionsReq) (resp *msg.OperateMessageListReactionExtensionsResp, err error) {
+	return
+}
+
+func (rpc *rpcChat) AddMessageReactionExtensions(ctx context.Context, req *msg.ModifyMessageReactionExtensionsReq) (resp *msg.ModifyMessageReactionExtensionsResp, err error) {
+	return
+}
+
+func (rpc *rpcChat) DeleteMessageReactionExtensions(ctx context.Context, req *msg.OperateMessageListReactionExtensionsReq) (resp *msg.OperateMessageListReactionExtensionsResp, err error) {
+	return
+}
