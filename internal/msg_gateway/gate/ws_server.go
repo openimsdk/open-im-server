16a7bc63f2a3f93467fa1e750d88933d920424b0
Gordon <1432970085@qq.com>
2022-12-22 17:37:34 +0800

    callback update

--- internal/msg_gateway/gate/ws_server.go
+++ internal/msg_gateway/gate/ws_server.go
@@ -388,6 +388,9 @@ func (ws *WServer) delUserConn(conn *UserConn) {
 	if err != nil {
 		log.Error(operationID, " close err", "", "uid", uid, "platform", platform)
 	}
+	if conn.PlatformID == 0 || conn.connID == "" {
+		log.NewWarn(operationID, utils.GetSelfFuncName(), "PlatformID or connID is null", conn.PlatformID, conn.connID)
+	}
 	callbackResp := callbackUserOffline(operationID, conn.userID, int(conn.PlatformID), conn.connID)
 	if callbackResp.ErrCode != 0 {
 		log.NewError(operationID, utils.GetSelfFuncName(), "callbackUserOffline failed", callbackResp)
