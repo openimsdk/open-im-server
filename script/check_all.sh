235be5da0cf900532011e06e46491fb0ed740878
wangchuxiao <wangchuxiao97@outlook.com>
2022-12-28 17:26:53 +0800

    cron

--- script/check_all.sh
+++ script/check_all.sh
@@ -54,13 +54,13 @@ else
 fi
 
 
-#check=$(ps aux | grep -w ./${cron_task_name} | grep -v grep | wc -l)
-#if [ $check -ge 1 ]; then
-#  echo -e ${GREEN_PREFIX}"none  port has been listening,belongs service is openImCronTask"${COLOR_SUFFIX}
-#else
-#  echo -e ${RED_PREFIX}"cron_task_name service does not start normally"${COLOR_SUFFIX}
-#        echo -e ${RED_PREFIX}"please check ../logs/openIM.log "${COLOR_SUFFIX}
-#      exit -1
-#fi
-#
-#echo -e ${YELLOW_PREFIX}"all services launch success"${COLOR_SUFFIX}
+check=$(ps aux | grep -w ./${cron_task_name} | grep -v grep | wc -l)
+if [ $check -ge 1 ]; then
+ echo -e ${GREEN_PREFIX}"none  port has been listening,belongs service is openImCronTask"${COLOR_SUFFIX}
+else
+ echo -e ${RED_PREFIX}"cron_task_name service does not start normally"${COLOR_SUFFIX}
+       echo -e ${RED_PREFIX}"please check ../logs/openIM.log "${COLOR_SUFFIX}
+     exit -1
+fi
+
+echo -e ${YELLOW_PREFIX}"all services launch success"${COLOR_SUFFIX}
