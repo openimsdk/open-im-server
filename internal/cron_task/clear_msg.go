1fdc4d06d1c5bf3ae7f4a6d9022e22892a02d5cb
wangchuxiao <wangchuxiao97@outlook.com>
2022-12-28 12:10:10 +0800

    test cron

--- internal/cron_task/clear_msg.go
+++ internal/cron_task/clear_msg.go
@@ -135,7 +135,7 @@ func deleteMongoMsg(operationID string, ID string, index int64, delStruct *delMs
 					return 0, err
 				}
 				if hasMarkDelFlag {
-					log.NewInfo(operationID, ID, "hasMarkDelFlag", "index:", index, "msgPb:", msgPb, msgs.UID, msgs.Msg)
+					log.NewInfo(operationID, ID, "hasMarkDelFlag", "index:", index, "msgPb:", msgPb, msgs.UID, msgs.Msg[0].SendTime)
 					if err := db.DB.UpdateOneMsgList(msgs); err != nil {
 						return delStruct.getSetMinSeq(), utils.Wrap(err, "")
 					}
