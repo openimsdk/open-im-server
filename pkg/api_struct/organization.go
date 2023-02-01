abd957a45907c16dee6c0b296c73a88ec22f1cd0
skiffer-git <44203734@qq.com>
2023-02-01 12:16:14 +0800

    Error code standardization

---
+++ pkg/api_struct/organization.go
@@ -0,0 +1,121 @@
+package api_struct
+
+import open_im_sdk "Open_IM/pkg/proto/sdk_ws"
+
+type CreateDepartmentReq struct {
+	*open_im_sdk.Department
+	OperationID string `json:"operationID" binding:"required"`
+}
+type CreateDepartmentResp struct {
+	CommResp
+	Department *open_im_sdk.Department `json:"-"`
+	Data       map[string]interface{}  `json:"data" swaggerignore:"true"`
+}
+
+type UpdateDepartmentReq struct {
+	*open_im_sdk.Department
+	DepartmentID string `json:"departmentID" binding:"required"`
+	OperationID  string `json:"operationID" binding:"required"`
+}
+type UpdateDepartmentResp struct {
+	CommResp
+}
+
+type GetSubDepartmentReq struct {
+	OperationID  string `json:"operationID" binding:"required"`
+	DepartmentID string `json:"departmentID" binding:"required"`
+}
+type GetSubDepartmentResp struct {
+	CommResp
+	DepartmentList []*open_im_sdk.Department `json:"-"`
+	Data           []map[string]interface{}  `json:"data" swaggerignore:"true"`
+}
+
+type DeleteDepartmentReq struct {
+	OperationID  string `json:"operationID" binding:"required"`
+	DepartmentID string `json:"departmentID" binding:"required"`
+}
+type DeleteDepartmentResp struct {
+	CommResp
+}
+
+type CreateOrganizationUserReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+	*open_im_sdk.OrganizationUser
+}
+type CreateOrganizationUserResp struct {
+	CommResp
+}
+
+type UpdateOrganizationUserReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+	*open_im_sdk.OrganizationUser
+}
+type UpdateOrganizationUserResp struct {
+	CommResp
+}
+
+type CreateDepartmentMemberReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+	*open_im_sdk.DepartmentMember
+}
+
+type CreateDepartmentMemberResp struct {
+	CommResp
+}
+
+type GetUserInDepartmentReq struct {
+	UserID      string `json:"userID" binding:"required"`
+	OperationID string `json:"operationID" binding:"required"`
+}
+type GetUserInDepartmentResp struct {
+	CommResp
+	UserInDepartment *open_im_sdk.UserInDepartment `json:"-"`
+	Data             map[string]interface{}        `json:"data" swaggerignore:"true"`
+}
+
+type UpdateUserInDepartmentReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+	*open_im_sdk.DepartmentMember
+}
+type UpdateUserInDepartmentResp struct {
+	CommResp
+}
+
+type DeleteOrganizationUserReq struct {
+	UserID      string `json:"userID" binding:"required"`
+	OperationID string `json:"operationID" binding:"required"`
+}
+type DeleteOrganizationUserResp struct {
+	CommResp
+}
+
+type GetDepartmentMemberReq struct {
+	DepartmentID string `json:"departmentID" binding:"required"`
+	OperationID  string `json:"operationID" binding:"required"`
+}
+type GetDepartmentMemberResp struct {
+	CommResp
+	UserInDepartmentList []*open_im_sdk.UserDepartmentMember `json:"-"`
+	Data                 []map[string]interface{}            `json:"data" swaggerignore:"true"`
+}
+
+type DeleteUserInDepartmentReq struct {
+	DepartmentID string `json:"departmentID" binding:"required"`
+	UserID       string `json:"userID" binding:"required"`
+	OperationID  string `json:"operationID" binding:"required"`
+}
+type DeleteUserInDepartmentResp struct {
+	CommResp
+}
+
+type GetUserInOrganizationReq struct {
+	OperationID string   `json:"operationID" binding:"required"`
+	UserIDList  []string `json:"userIDList" binding:"required"`
+}
+
+type GetUserInOrganizationResp struct {
+	CommResp
+	OrganizationUserList []*open_im_sdk.OrganizationUser `json:"-"`
+	Data                 []map[string]interface{}        `json:"data" swaggerignore:"true"`
+}
---
+++ pkg/api_struct/user.go
@@ -0,0 +1,58 @@
+package api_struct
+
+import (
+	open_im_sdk "Open_IM/pkg/proto/sdk_ws"
+)
+
+type GetUsersInfoReq struct {
+	OperationID string   `json:"operationID" binding:"required"`
+	UserIDList  []string `json:"userIDList" binding:"required"`
+}
+type GetUsersInfoResp struct {
+	CommResp
+	UserInfoList []*open_im_sdk.PublicUserInfo `json:"-"`
+	Data         []map[string]interface{}      `json:"data" swaggerignore:"true"`
+}
+
+type UpdateSelfUserInfoReq struct {
+	ApiUserInfo
+	OperationID string `json:"operationID" binding:"required"`
+}
+type SetGlobalRecvMessageOptReq struct {
+	OperationID      string `json:"operationID" binding:"required"`
+	GlobalRecvMsgOpt *int32 `json:"globalRecvMsgOpt" binding:"omitempty,oneof=0 1 2"`
+}
+type SetGlobalRecvMessageOptResp struct {
+	CommResp
+}
+type UpdateUserInfoResp struct {
+	CommResp
+}
+
+type GetSelfUserInfoReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+	UserID      string `json:"userID" binding:"required"`
+}
+type GetSelfUserInfoResp struct {
+	CommResp
+	UserInfo *open_im_sdk.UserInfo  `json:"-"`
+	Data     map[string]interface{} `json:"data" swaggerignore:"true"`
+}
+
+type GetFriendIDListFromCacheReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+}
+
+type GetFriendIDListFromCacheResp struct {
+	CommResp
+	UserIDList []string `json:"userIDList" binding:"required"`
+}
+
+type GetBlackIDListFromCacheReq struct {
+	OperationID string `json:"operationID" binding:"required"`
+}
+
+type GetBlackIDListFromCacheResp struct {
+	CommResp
+	UserIDList []string `json:"userIDList" binding:"required"`
+}
