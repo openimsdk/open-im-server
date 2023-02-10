1e1c6de56e9ce81d87bc7cfaff5cc8b338842e38
withchao <993506633@qq.com>
2023-02-10 15:36:25 +0800

    group nickname

---
+++ pkg/common/db/table/relation/utils.go
@@ -0,0 +1,12 @@
+package relation
+
+type BatchUpdateGroupMember struct {
+	GroupID string
+	UserID  string
+	Map     map[string]any
+}
+
+type GroupSimpleUserID struct {
+	Hash      uint64
+	MemberNum uint32
+}
