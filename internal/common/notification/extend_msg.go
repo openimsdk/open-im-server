ec7f44358c68d90872469a2ac6e712f8490fcc1f
skiffer-git <44203734@qq.com>
2023-02-13 09:14:50 +0800

    Error code standardization

---
+++ internal/common/notification/extend_msg.go
@@ -0,0 +1,100 @@
+package notification
+
+import (
+	"Open_IM/pkg/common/config"
+	"Open_IM/pkg/common/constant"
+	"Open_IM/pkg/common/log"
+	"Open_IM/pkg/proto/msg"
+	sdkws "Open_IM/pkg/proto/sdkws"
+	"Open_IM/pkg/utils"
+	"context"
+)
+
+func ExtendMessageUpdatedNotification(operationID, sendID string, sourceID string, sessionType int32,
+	req *msg.SetMessageReactionExtensionsReq, resp *msg.SetMessageReactionExtensionsResp, isHistory bool, isReactionFromCache bool) {
+	var m apistruct.ReactionMessageModifierNotification
+	m.SourceID = req.SourceID
+	m.OpUserID = req.OpUserID
+	m.SessionType = req.SessionType
+	keyMap := make(map[string]*sdkws.KeyValue)
+	for _, valueResp := range resp.Result {
+		if valueResp.ErrCode == 0 {
+			keyMap[valueResp.KeyValue.TypeKey] = valueResp.KeyValue
+		}
+	}
+	if len(keyMap) == 0 {
+		log.NewWarn(operationID, "all key set failed can not send notification", *req)
+		return
+	}
+	m.SuccessReactionExtensionList = keyMap
+	m.ClientMsgID = req.ClientMsgID
+	m.IsReact = resp.IsReact
+	m.IsExternalExtensions = req.IsExternalExtensions
+	m.MsgFirstModifyTime = resp.MsgFirstModifyTime
+	messageReactionSender(operationID, sendID, sourceID, sessionType, constant.ReactionMessageModifier, utils.StructToJsonString(m), isHistory, isReactionFromCache)
+}
+func ExtendMessageDeleteNotification(operationID, sendID string, sourceID string, sessionType int32,
+	req *msg.DeleteMessageListReactionExtensionsReq, resp *msg.DeleteMessageListReactionExtensionsResp, isHistory bool, isReactionFromCache bool) {
+	var m apistruct.ReactionMessageDeleteNotification
+	m.SourceID = req.SourceID
+	m.OpUserID = req.OpUserID
+	m.SessionType = req.SessionType
+	keyMap := make(map[string]*sdkws.KeyValue)
+	for _, valueResp := range resp.Result {
+		if valueResp.ErrCode == 0 {
+			keyMap[valueResp.KeyValue.TypeKey] = valueResp.KeyValue
+		}
+	}
+	if len(keyMap) == 0 {
+		log.NewWarn(operationID, "all key set failed can not send notification", *req)
+		return
+	}
+	m.SuccessReactionExtensionList = keyMap
+	m.ClientMsgID = req.ClientMsgID
+	m.MsgFirstModifyTime = req.MsgFirstModifyTime
+
+	messageReactionSender(operationID, sendID, sourceID, sessionType, constant.ReactionMessageDeleter, utils.StructToJsonString(m), isHistory, isReactionFromCache)
+}
+func messageReactionSender(operationID, sendID string, sourceID string, sessionType, contentType int32, content string, isHistory bool, isReactionFromCache bool) {
+	options := make(map[string]bool, 5)
+	utils.SetSwitchFromOptions(options, constant.IsOfflinePush, false)
+	utils.SetSwitchFromOptions(options, constant.IsConversationUpdate, false)
+	utils.SetSwitchFromOptions(options, constant.IsSenderConversationUpdate, false)
+	utils.SetSwitchFromOptions(options, constant.IsUnreadCount, false)
+	utils.SetSwitchFromOptions(options, constant.IsReactionFromCache, isReactionFromCache)
+	if !isHistory {
+		utils.SetSwitchFromOptions(options, constant.IsHistory, false)
+		utils.SetSwitchFromOptions(options, constant.IsPersistent, false)
+	}
+	pbData := msg.SendMsgReq{
+		OperationID: operationID,
+		MsgData: &sdkws.MsgData{
+			SendID:      sendID,
+			ClientMsgID: utils.GetMsgID(sendID),
+			SessionType: sessionType,
+			MsgFrom:     constant.SysMsgType,
+			ContentType: contentType,
+			Content:     []byte(content),
+			//	ForceList:        params.ForceList,
+			CreateTime: utils.GetCurrentTimestampByMill(),
+			Options:    options,
+		},
+	}
+	switch sessionType {
+	case constant.SingleChatType, constant.NotificationChatType:
+		pbData.MsgData.RecvID = sourceID
+	case constant.GroupChatType, constant.SuperGroupChatType:
+		pbData.MsgData.GroupID = sourceID
+	}
+	etcdConn, err := rpc.GetConn(context.Background(), config.Config.RpcRegisterName.OpenImMsgName)
+	if err != nil {
+		return
+	}
+	client := msg.NewMsgClient(etcdConn)
+	reply, err := client.SendMsg(context.Background(), &pbData)
+	if err != nil {
+		log.NewError(operationID, "SendMsg rpc failed, ", pbData.String(), err.Error())
+	} else if reply.ErrCode != 0 {
+		log.NewError(operationID, "SendMsg rpc failed, ", pbData.String(), reply.ErrCode, reply.ErrMsg)
+	}
+}
---
+++ internal/common/notification/msg.go
@@ -0,0 +1,48 @@
+package notification
+
+import (
+	"Open_IM/internal/rpc/msg"
+	"Open_IM/pkg/common/constant"
+	"Open_IM/pkg/common/log"
+	sdkws "Open_IM/pkg/proto/sdkws"
+	"Open_IM/pkg/utils"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+)
+
+func DeleteMessageNotification(opUserID, userID string, seqList []uint32, operationID string) {
+	DeleteMessageTips := sdkws.DeleteMessageTips{OpUserID: opUserID, UserID: userID, SeqList: seqList}
+	MessageNotification(operationID, userID, userID, constant.DeleteMessageNotification, &DeleteMessageTips)
+}
+
+func MessageNotification(operationID, sendID, recvID string, contentType int32, m proto.Message) {
+	log.Debug(operationID, utils.GetSelfFuncName(), "args: ", m.String(), contentType)
+	var err error
+	var tips sdkws.TipsComm
+	tips.Detail, err = proto.Marshal(m)
+	if err != nil {
+		log.Error(operationID, "Marshal failed ", err.Error(), m.String())
+		return
+	}
+
+	marshaler := jsonpb.Marshaler{
+		OrigName:     true,
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+	}
+
+	tips.JsonDetail, _ = marshaler.MarshalToString(m)
+	var n msg.NotificationMsg
+	n.SendID = sendID
+	n.RecvID = recvID
+	n.ContentType = contentType
+	n.SessionType = constant.SingleChatType
+	n.MsgFrom = constant.SysMsgType
+	n.OperationID = operationID
+	n.Content, err = proto.Marshal(&tips)
+	if err != nil {
+		log.Error(operationID, "Marshal failed ", err.Error(), tips.String())
+		return
+	}
+	msg.Notification(&n)
+}
