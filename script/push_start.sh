74ba5ba614f51393f2741fd0ab182255bb4cced9
wangchuxiao <wangchuxiao97@outlook.com>
2023-03-08 18:30:20 +0800

    script

--- script/push_start.sh
+++ script/push_start.sh
@@ -25,7 +25,7 @@ sleep 1
 cd ${push_binary_root}
 
 for ((i = 0; i < ${#rpc_ports[@]}; i++)); do
-  nohup ./${push_name} -port ${rpc_ports[$i]} -prometheus_port ${prome_ports[$i]} >>../logs/openIM.log 2>&1 &
+  nohup ./${push_name} --port ${rpc_ports[$i]} --prometheus_port ${prome_ports[$i]} >>../logs/openIM.log 2>&1 &
 done
 
 sleep 3
