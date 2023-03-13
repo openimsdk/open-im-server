fed4dbd54fe13da2ba638fdf7b8ed53814558cc5
Gordon <1432970085@qq.com>
2023-03-13 13:31:40 +0800

    zk add client options

--- internal/startrpc/start.go
+++ internal/startrpc/start.go
@@ -31,7 +31,7 @@ func Start(rpcPort int, rpcRegisterName string, prometheusPort int, rpcFn func(c
 		return utils.Wrap1(err)
 	}
 	defer zkClient.Close()
-	zkClient.AddOption(grpc.WithTransportCredentials(insecure.NewCredentials()))
+	zkClient.AddOption(grpc.WithTransportCredentials(insecure.NewCredentials()), mw.GrpcClient())
 	registerIP, err := network.GetRpcRegisterIP(config.Config.RpcRegisterIP)
 	if err != nil {
 		return err
