cf968c0b4c9c505e1817431b8b67e98de5c8d02b
wangchuxiao <wangchuxiao97@outlook.com>
2023-03-17 11:41:20 +0800

    friend

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+
 	"github.com/OpenIMSDK/Open-IM-Server/internal/api/a2r"
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/config"
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/discoveryregistry"
