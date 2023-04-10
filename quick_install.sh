97c1b235b5829da8b4ea337c98fd9779f972030d
wangchuxiao <wangchuxiao97@outlook.com>
2023-04-10 20:34:03 +0800

    script

--- quick_install.sh
+++ quick_install.sh
@@ -77,9 +77,9 @@ install_docker_compose() {
     read DATA_DIR
     is_empyt $DATA_DIR
     if [ $? -eq 1 ]; then 
-        DATA_DIR="."
+        DATA_DIR=$(pwd)
     fi 
-    
+    echo $DATA_DIR
     echo "Please enter the user, deault is root, press enter to use default"
     read USER
     is_empyt $USER
