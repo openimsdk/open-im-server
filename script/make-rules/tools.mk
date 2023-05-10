630e7e52961e7570bad504efb5107f13d661ecd6
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-05-11 01:34:15 +0800

    feat: refactor the makefile to initialize the base modules
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

---
+++ script/make-rules/tools.mk
@@ -0,0 +1,18 @@
+# Copyright © 2023 OpenIMSDK.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ==============================================================================
+# Makefile helper functions for tools(https://github.com/avelino/awesome-go) -> DIR: {TOOT_DIR}/tools | (go >= 1.19)
+# Why download to the tools directory, thinking we might often switch Go versions using gvm.
+#
