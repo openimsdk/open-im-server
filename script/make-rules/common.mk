1a034cfc660e8204496e4b1d9375ec368bf3dc01
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-05-13 16:04:20 +0800

    feat(make): define common githook
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

--- script/make-rules/common.mk
+++ script/make-rules/common.mk
@@ -119,6 +119,9 @@ ifndef V
 MAKEFLAGS += --no-print-directory
 endif
 
+# Copy githook scripts when execute makefile
+COPY_GITHOOK:=$(shell cp -f githooks/* .git/hooks/)
+
 # COMMA: Concatenate multiple strings to form a list of strings
 COMMA := ,
 # SPACE: Used to separate strings
