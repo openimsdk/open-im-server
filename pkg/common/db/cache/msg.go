2358ec3d9098d9de7822b22fe8d8146741c37de8
wangchuxiao <wangchuxiao97@outlook.com>
2023-05-18 15:04:11 +0800

    get seqs

--- pkg/common/db/cache/msg.go
+++ pkg/common/db/cache/msg.go
@@ -137,7 +137,10 @@ func (c *msgCache) getSeqs(ctx context.Context, items []string, getkey func(s st
 		if seq.Err() != nil && seq.Err() != redis.Nil {
 			return nil, errs.Wrap(v.Err())
 		}
-		m[items[i]] = utils.StringToInt64(seq.Val())
+		seqInt64 := utils.StringToInt64(seq.Val())
+		if seqInt64 != 0 {
+			m[items[i]] = seqInt64
+		}
 	}
 	return m, nil
 }
