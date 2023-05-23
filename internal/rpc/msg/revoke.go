724d8bcd97a189311b06ba15c13ef23fe9e4aa06
withchao <993506633@qq.com>
2023-05-23 15:29:44 +0800

    RevokeMsgReq pb

--- internal/rpc/msg/revoke.go
+++ internal/rpc/msg/revoke.go
@@ -2,13 +2,19 @@ package msg
 
 import (
 	"context"
-
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/proto/msg"
 )
 
 func (m *msgServer) RevokeMsg(ctx context.Context, req *msg.RevokeMsgReq) (*msg.RevokeMsgResp, error) {
-	// if _, err := m.MsgDatabase.DelMsgBySeqs(ctx, req.UserID, req.Seqs); err != nil {
-	// 	return nil, err
-	// }
+	//msgs := []*sdkws.MsgData{}
+	//if err := m.MsgDatabase.MsgToMongoMQ(ctx, "", req.ConversationID, msgs, 0); err != nil {
+	//	return nil, err
+	//}
+	//msg := sdkws.MsgData{
+	//	SendID: "",
+	//}
+	//
+	//m.SendMsg(ctx, &msg.SendMsgReq{})
+
 	return &msg.RevokeMsgResp{}, nil
 }
