290d7cffde76ad0370f8f942c780f091a26f27f6
wangchuxiao <wangchuxiao97@outlook.com>
2023-05-29 18:04:22 +0800

    grpc conn

--- pkg/rpcclient/black.go
+++ pkg/rpcclient/black.go
@@ -6,23 +6,24 @@ import (
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/config"
 	discoveryRegistry "github.com/OpenIMSDK/Open-IM-Server/pkg/discoveryregistry"
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/proto/friend"
+	"google.golang.org/grpc"
 )
 
 type BlackClient struct {
-	*MetaClient
+	conn *grpc.ClientConn
 }
 
-func NewBlackClient(zk discoveryRegistry.SvcDiscoveryRegistry) *BlackClient {
-	return &BlackClient{NewMetaClient(zk, config.Config.RpcRegisterName.OpenImFriendName)}
+func NewBlackClient(discov discoveryRegistry.SvcDiscoveryRegistry) *BlackClient {
+	conn, err := discov.GetConn(context.Background(), config.Config.RpcRegisterName.OpenImFriendName)
+	if err != nil {
+		panic(err)
+	}
+	return &BlackClient{conn: conn}
 }
 
 // possibleBlackUserID是否被userID拉黑，也就是是否在userID的黑名单中
 func (b *BlackClient) IsBlocked(ctx context.Context, possibleBlackUserID, userID string) (bool, error) {
-	cc, err := b.getConn(ctx)
-	if err != nil {
-		return false, err
-	}
-	r, err := friend.NewFriendClient(cc).IsBlack(ctx, &friend.IsBlackReq{UserID1: possibleBlackUserID, UserID2: userID})
+	r, err := friend.NewFriendClient(b.conn).IsBlack(ctx, &friend.IsBlackReq{UserID1: possibleBlackUserID, UserID2: userID})
 	if err != nil {
 		return false, err
 	}
