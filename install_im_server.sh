cbb4d7f5ea846359edc4ff4a7736a33e834fccf5
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-06-10 16:38:18 +0800

    feat: automated deployment script
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- install_im_server.sh
+++ install_im_server.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
-internet_ip=`curl ifconfig.me -s`
-echo $internet_ip
 
+# Get the external IP address
+internet_ip=$(curl -s ifconfig.me)
+
+# Print the IP address
+echo "Internet IP address: ${internet_ip}"
+
+# Read environment variables
 source .env
-echo $MINIO_ENDPOINT
-if [ $MINIO_ENDPOINT == "http://127.0.0.1:10005" ]; then
-	sed -i "s/127.0.0.1/${internet_ip}/" .env 
 
+# Check if MINIO_ENDPOINT variable is set correctly
+if [[ -z "${MINIO_ENDPOINT}" || "${MINIO_ENDPOINT}" != "http://127.0.0.1:10005" ]]; then
+  echo "Error: MINIO_ENDPOINT is not set or is not equal to http://127.0.0.1:10005."
+  exit 1
 fi
-	
-cd script ;
-chmod +x *.sh ;
+
+# Replace the IP address in the .env file
+sed -i "s/127.0.0.1/${internet_ip}/" .env
+
+# Enter the script directory
+cd script || exit 1
+
+# Add execute permission to scripts
+chmod +x *.sh
+
+# Initialize password
 ./init_pwd.sh
-./env_check.sh;
-cd .. ;
-docker-compose up -d;
-cd script ;
+
+# Check environment variables
+./env_check.sh
+
+# Return to the parent directory
+cd ..
+
+# Start docker containers
+docker-compose up -d
+
+# Enter the script directory
+cd script || exit 1
+
+# Check if docker services are running properly
 ./docker_check_service.sh
