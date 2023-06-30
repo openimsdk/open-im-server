a3636a282a4f4a6b0457c8c060ce9a97d3ea146e
kubbot & kubecub <3293172751ysy@gmail.com>
2023-06-30 21:47:20 +0800

    fix
    Signed-off-by: kubbot & kubecub <3293172751ysy@gmail.com>

--- pkg/common/db/controller/storage.go
+++ pkg/common/db/controller/storage.go
@@ -1,17 +1,3 @@
-// Copyright © 2023 OpenIM. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package controller
 
 import "C"
@@ -119,6 +105,7 @@ func (c *s3Database) urlName(name string) string {
 		Host:        c.url.Host,
 		Path:        c.url.Path,
 		RawPath:     c.url.RawPath,
+		OmitHost:    c.url.OmitHost,
 		ForceQuery:  c.url.ForceQuery,
 		RawQuery:    c.url.RawQuery,
 		Fragment:    c.url.Fragment,
