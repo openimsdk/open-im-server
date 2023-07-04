233a21874be6b1522fc49741341d5ac2f77a50fa
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-07-04 18:35:10 +0800

    feat: add license
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- scripts/make-rules/golang.mk
+++ scripts/make-rules/golang.mk
@@ -65,7 +65,7 @@ ifeq (${BINS},)
 endif
 
 # TODO: EXCLUDE_TESTS variable, which contains the name of the package to be excluded from the test
-EXCLUDE_TESTS=github.com/OpenIMSDK/Open-IM-Server/test github.com/OpenIMSDK/Open-IM-Server/pkg/log github.com/OpenIMSDK/Open-IM-Server/db github.com/OpenIMSDK/Open-IM-Server/scripts github.com/OpenIMSDK/Open-IM-Server/deploy_k8s github.com/OpenIMSDK/Open-IM-Server/deploy github.com/OpenIMSDK/Open-IM-Server/config
+EXCLUDE_TESTS=github.com/OpenIMSDK/Open-IM-Server/test github.com/OpenIMSDK/Open-IM-Server/pkg/log github.com/OpenIMSDK/Open-IM-Server/db github.com/OpenIMSDK/Open-IM-Server/scripts github.com/OpenIMSDK/Open-IM-Server/config
 
 # ==============================================================================
 # ❯ tree -L 1 cmd
