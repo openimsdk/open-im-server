f7bd391ed75e266108b17f12f8b5155609ac7528
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-07-07 12:03:20 +0800

    fix: optimization details
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- scripts/lib/color.sh
+++ scripts/lib/color.sh
@@ -14,18 +14,18 @@
 # limitations under the License.
 
 
-#Define color variables
-#Feature
+# Define color variables
+# Feature
 COLOR_NORMAL='\033[0m';COLOR_BOLD='\033[1m';COLOR_DIM='\033[2m';COLOR_UNDER='\033[4m';
 COLOR_ITALIC='\033[3m';COLOR_NOITALIC='\033[23m';COLOR_BLINK='\033[5m';
 COLOR_REVERSE='\033[7m';COLOR_CONCEAL='\033[8m';COLOR_NOBOLD='\033[22m';
 COLOR_NOUNDER='\033[24m';COLOR_NOBLINK='\033[25m';
 
-#Front color
+# Front color
 COLOR_BLACK='\033[30m';COLOR_RED='\033[31m';COLOR_GREEN='\033[32m';COLOR_YELLOW='\033[33m';
 COLOR_BLUE='\033[34m';COLOR_MAGENTA='\033[35m';COLOR_CYAN='\033[36m';COLOR_WHITE='\033[37m';
 
-#background color
+# background color
 COLOR_BBLACK='\033[40m';COLOR_BRED='\033[41m';
 COLOR_BGREEN='\033[42m';COLOR_BYELLOW='\033[43m';
 COLOR_BBLUE='\033[44m';COLOR_BMAGENTA='\033[45m';
