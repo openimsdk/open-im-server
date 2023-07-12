a23ecb1710337e802edf9c035bd6bf79283f414c
wangchuxiao <wangchuxiao97@outlook.com>
2023-07-12 11:33:08 +0800

    cron add log and fix cron

--- internal/tools/cron_task.go
+++ internal/tools/cron_task.go
@@ -11,7 +11,6 @@ import (
 )
 
 func StartCronTask() error {
-	log.ZInfo(context.Background(), "start cron task", "cron config", config.Config.ChatRecordsClearTime)
 	fmt.Println("cron task start, config", config.Config.ChatRecordsClearTime)
 	msgTool, err := InitMsgTool()
 	if err != nil {
@@ -20,11 +19,13 @@ func StartCronTask() error {
 	c := cron.New()
 	var wg sync.WaitGroup
 	wg.Add(1)
+	log.ZInfo(context.Background(), "start chatRecordsClearTime cron task", "cron config", config.Config.ChatRecordsClearTime)
 	_, err = c.AddFunc(config.Config.ChatRecordsClearTime, msgTool.AllConversationClearMsgAndFixSeq)
 	if err != nil {
 		fmt.Println("start allConversationClearMsgAndFixSeq cron failed", err.Error(), config.Config.ChatRecordsClearTime)
 		panic(err)
 	}
+	log.ZInfo(context.Background(), "start msgDestruct cron task", "cron config", config.Config.ChatRecordsClearTime)
 	_, err = c.AddFunc(config.Config.MsgDestructTime, msgTool.ConversationsDestructMsgs)
 	if err != nil {
 		fmt.Println("start conversationsDestructMsgs cron failed", err.Error(), config.Config.ChatRecordsClearTime)
