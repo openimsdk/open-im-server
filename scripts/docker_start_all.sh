9df16eaf258cba80269d00d7acbf14b540723323
hanzhixiao <709674996@qq.com>
2023-08-10 10:50:05 +0800

    new_815
    Signed-off-by: hanzhixiao <709674996@qq.com>

--- scripts/docker_start_all.sh
+++ scripts/docker_start_all.sh
@@ -30,6 +30,15 @@ need_to_start_server_shell=(
   ${SCRIPTS_ROOT}/start_cron.sh
 )
 
+component_check=start_component_check.sh
+chmod +x $SCRIPTS_ROOT/$component_check
+$SCRIPTS_ROOT/$component_check
+if [ $? -ne 0 ]; then
+  # Print error message and exit
+  echo "${BOLD_PREFIX}${RED_PREFIX}Error executing ${component_check}. Exiting...${COLOR_SUFFIX}"
+  exit -1
+fi
+
 #fixme The 10 second delay to start the project is for the docker-compose one-click to start openIM when the infrastructure dependencies are not started
 
 sleep 10
