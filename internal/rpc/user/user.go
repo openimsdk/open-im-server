8010bd94f7c5da0f3d0bf5c780376598b7e4e50f
pluto <2631223275@qq.com>
2023-08-11 17:11:37 +0800

    update note

--- internal/rpc/user/user.go
+++ internal/rpc/user/user.go
@@ -274,7 +274,7 @@ func (s *userServer) SubscribeOrCancelUsersStatus(ctx context.Context, req *pbus
 	return &pbuser.SubscribeOrCancelUsersStatusResp{}, nil
 }
 
-// GetUserStatus Get the online status of the user
+// GetUserStatus Get the online status of the user.
 func (s *userServer) GetUserStatus(ctx context.Context, req *pbuser.GetUserStatusReq) (resp *pbuser.GetUserStatusResp, err error) {
 	onlineStatusList, err := s.UserDatabase.GetUserStatus(ctx, req.UserIDs)
 	if err != nil {
@@ -283,7 +283,7 @@ func (s *userServer) GetUserStatus(ctx context.Context, req *pbuser.GetUserStatu
 	return &pbuser.GetUserStatusResp{StatusList: onlineStatusList}, nil
 }
 
-// SetUserStatus Synchronize user's online status
+// SetUserStatus Synchronize user's online status.
 func (s *userServer) SetUserStatus(ctx context.Context, req *pbuser.SetUserStatusReq) (resp *pbuser.SetUserStatusResp, err error) {
 	err = s.UserDatabase.SetUserStatus(ctx, req.StatusList)
 	if err != nil {
@@ -307,7 +307,7 @@ func (s *userServer) SetUserStatus(ctx context.Context, req *pbuser.SetUserStatu
 	return &pbuser.SetUserStatusResp{}, nil
 }
 
-// GetSubscribeUsersStatus Get the online status of subscribers
+// GetSubscribeUsersStatus Get the online status of subscribers.
 func (s *userServer) GetSubscribeUsersStatus(ctx context.Context, req *pbuser.GetSubscribeUsersStatusReq) (*pbuser.GetSubscribeUsersStatusResp, error) {
 	userList, err := s.UserDatabase.GetAllSubscribeList(ctx, req.UserID)
 	if err != nil {
