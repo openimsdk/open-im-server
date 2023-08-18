198c49b493d34df57f06ddf886b69f79edbab134
pluto <2631223275@qq.com>
2023-08-18 10:41:57 +0800

    mysql conversion

--- test/data-conversion/kafka-conversation.go
+++ test/data-conversion/kafka-conversation.go
@@ -1,28 +1,13 @@
-// Copyright © 2023 OpenIM. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package data_conversion
 
 import (
 	"fmt"
-
 	"github.com/Shopify/sarama"
 )
 
 var (
 	topic = "latestMsgToRedis"
-	addr  = "127.0.0.1:9092"
+	addr  = "43.128.72.19:9092"
 )
 
 var consumer sarama.Consumer
