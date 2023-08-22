7b29663a4c6ce49e0a98badb94b5dc91876805c8
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-08-22 18:51:35 +0800

    fix: Add some optimizations
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- scripts/build-all-service.sh
+++ scripts/build-all-service.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 # This script runs `make build` command.
 # The command compiles all Makefile configs.
 # Args:
