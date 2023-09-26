78912cf4557f38e0f285d65550e0e93d86d88cd7
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-09-26 20:18:52 +0800

    feat: add openim task list
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

--- scripts/install/environment.sh
+++ scripts/install/environment.sh
@@ -69,6 +69,15 @@ def "ENV_FILE" ""${OPENIM_ROOT}"/scripts/install/environment.sh"
 def "CHAT_BRANCH" "main"
 def "SERVER_BRANCH" "main"
 
+# Choose the appropriate image address, the default is GITHUB image, 
+# you can choose docker hub, for Chinese users can choose Ali Cloud
+# export IMAGE_REGISTRY="ghcr.io/openimsdk"
+# export IMAGE_REGISTRY="openim"
+# export IMAGE_REGISTRY="registry.cn-hangzhou.aliyuncs.com/openimsdk"
+def "IMAGE_REGISTRY" "ghcr.io/openimsdk"
+# def "IMAGE_REGISTRY" "openim"
+# def "IMAGE_REGISTRY" "registry.cn-hangzhou.aliyuncs.com/openimsdk"
+
 ###################### OpenIM Docker Network ######################
 # 设置 Docker 网络的网段
 readonly DOCKER_BRIDGE_SUBNET=${DOCKER_BRIDGE_SUBNET:-'172.28.0.0/16'}
@@ -289,11 +298,16 @@ def "WEBSOCKET_TIMEOUT" "10"          # Websocket超时
 def "PUSH_ENABLE" "getui"             # 推送是否启用
 # GeTui推送URL
 readonly GETUI_PUSH_URL=${GETUI_PUSH_URL:-'https://restapi.getui.com/v2/$appId'}
+def "GETUI_MASTER_SECRET" ""          # GeTui主密钥
+def "GETUI_APP_KEY" ""                # GeTui应用密钥
+def "GETUI_INTENT" ""                 # GeTui推送意图
+def "GETUI_CHANNEL_ID" ""             # GeTui渠道ID
+def "GETUI_CHANNEL_NAME" ""           # GeTui渠道名称
 def "FCM_SERVICE_ACCOUNT" "x.json"    # FCM服务账户
-def "JPNS_APP_KEY"                    # JPNS应用密钥
-def "JPNS_MASTER_SECRET"              # JPNS主密钥
-def "JPNS_PUSH_URL"                   # JPNS推送URL
-def "JPNS_PUSH_INTENT"                # JPNS推送意图
+def "JPNS_APP_KEY"  ""                  # JPNS应用密钥
+def "JPNS_MASTER_SECRET" ""             # JPNS主密钥
+def "JPNS_PUSH_URL"  ""                 # JPNS推送URL
+def "JPNS_PUSH_INTENT" ""               # JPNS推送意图
 def "MANAGER_USERID_1" "openIM123456" # 管理员ID 1
 def "MANAGER_USERID_2" "openIM654321" # 管理员ID 2
 def "MANAGER_USERID_3" "openIMAdmin"  # 管理员ID 3
