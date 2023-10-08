697ffc222c9c8438a1bd2de85f4a6e218a9df6fb
withchao <993506633@qq.com>
2023-10-08 11:47:46 +0800

    feat: v2 to v3 data conversion

--- tools/data-conversion/chat/main.go
+++ tools/data-conversion/chat/main.go
@@ -9,19 +9,20 @@ import (
 )
 
 func main() {
-	const (
+	var (
 		usernameV2 = "root"
 		passwordV2 = "openIM123"
-		addrV2     = "127.0.0.1:3306"
+		addrV2     = "127.0.0.1:13306"
 		databaseV2 = "admin_chat"
 	)
 
-	const (
+	var (
 		usernameV3 = "root"
 		passwordV3 = "openIM123"
-		addrV3     = "127.0.0.1:3306"
+		addrV3     = "127.0.0.1:13306"
 		databaseV3 = "openim_enterprise"
 	)
+
 	dsnV2 := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", usernameV2, passwordV2, addrV2, databaseV2)
 	dsnV3 := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", usernameV3, passwordV3, addrV3, databaseV3)
 	dbV2, err := gorm.Open(mysql.Open(dsnV2), &gorm.Config{})
@@ -34,28 +35,32 @@ func main() {
 		log.Println("open v3 db failed", err)
 		return
 	}
-	var fns []func() error
 
-	Append := func(fn func() error) {
+	var fns []func() (string, error)
+
+	Append := func(fn func() (string, error)) {
 		fns = append(fns, fn)
 	}
 
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.Account) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.Attribute) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.Register) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.UserLoginRecord) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.Admin) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.Applet) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.ForbiddenAccount) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.InvitationRegister) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.IPForbidden) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.LimitUserLoginIP) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.RegisterAddFriend) })
-	Append(func() error { return conversion.FindAndInsert(dbV2, dbV3, conversion.RegisterAddGroup) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.Account) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.Attribute) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.Register) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.UserLoginRecord) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.Admin) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.Applet) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.ForbiddenAccount) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.InvitationRegister) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.IPForbidden) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.LimitUserLoginIP) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.RegisterAddFriend) })
+	Append(func() (string, error) { return conversion.FindAndInsert(dbV2, dbV3, conversion.RegisterAddGroup) })
 
 	for i := range fns {
-		if err := fns[i](); err != nil {
-			log.Printf("[%d] %s\n", i, err)
+		name, err := fns[i]()
+		if err == nil {
+			log.Printf("[%d/%d] %s success\n", i+1, len(fns), name)
+		} else {
+			log.Printf("[%d/%d] %s failed %s\n", i+1, len(fns), name, err)
 			return
 		}
 	}
