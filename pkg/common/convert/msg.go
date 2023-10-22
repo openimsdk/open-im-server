5d798c73ce74378bcda0b21d90987b991c36af47
cncsmonster <cncsmonster@163.com>
2023-10-23 00:10:39 +0800

    fix: fix lint erros in pkg/common/config,convert

--- pkg/common/convert/msg.go
+++ pkg/common/convert/msg.go
@@ -55,6 +55,7 @@ func MsgPb2DB(msg *sdkws.MsgData) *unrelation.MsgDataModel {
 	msgDataModel.AtUserIDList = msg.AtUserIDList
 	msgDataModel.AttachedInfo = msg.AttachedInfo
 	msgDataModel.Ex = msg.Ex
+
 	return &msgDataModel
 }
 
@@ -95,5 +96,6 @@ func MsgDB2Pb(msgModel *unrelation.MsgDataModel) *sdkws.MsgData {
 	msg.AtUserIDList = msgModel.AtUserIDList
 	msg.AttachedInfo = msgModel.AttachedInfo
 	msg.Ex = msgModel.Ex
+
 	return &msg
 }
