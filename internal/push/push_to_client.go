20623a826edbec396f91f49020dca3b14b65d585
a3d21 <a3d21@outlook.com>
2023-10-26 17:18:15 +0800

    fix: NotNotifyUser receive offline push

--- internal/push/push_to_client.go
+++ internal/push/push_to_client.go
@@ -222,7 +222,8 @@ func (p *Pusher) Push2SuperGroup(ctx context.Context, groupID string, msg *sdkws
 				// log.ZError(ctx, "GetRecvMsgNotNotifyUserIDs failed", err, "groupID", groupID)
 				return err
 			}
-			needOfflinePushUserIDs = utils.DifferenceString(notNotificationUserIDs, needOfflinePushUserIDs)
+			// todo => utils.ExceptString(needOfflinePushUserIDs, notNotificationUserIDs)
+			needOfflinePushUserIDs = utils.IntersectString(utils.DifferenceString(notNotificationUserIDs, needOfflinePushUserIDs), needOfflinePushUserIDs)
 		}
 		// Use offline push messaging
 		if len(needOfflinePushUserIDs) > 0 {
