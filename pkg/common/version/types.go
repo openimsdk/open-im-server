ad14bd1bd6b3d6eeca1ead37024ecfa9b2fd7969
go75 <go75@users.noreply.github.com>
2023-10-30 03:30:12 +0000

    cicd: robot automated Change

--- pkg/common/version/types.go
+++ pkg/common/version/types.go
@@ -4,15 +4,15 @@ package version
 // TODO: Add []string of api versions supported? It's still unclear
 // how we'll want to distribute that information.
 type Info struct {
-	Major      string `json:"major,omitempty"`
-	Minor      string `json:"minor,omitempty"`
-	GitVersion string `json:"gitVersion"`
+	Major        string `json:"major,omitempty"`
+	Minor        string `json:"minor,omitempty"`
+	GitVersion   string `json:"gitVersion"`
 	GitTreeState string `json:"gitTreeState,omitempty"`
-	GitCommit  string `json:"gitCommit,omitempty"`
-	BuildDate  string `json:"buildDate"`
-	GoVersion  string `json:"goVersion"`
-	Compiler   string `json:"compiler"`
-	Platform   string `json:"platform"`
+	GitCommit    string `json:"gitCommit,omitempty"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
 }
 
 type Output struct {
@@ -21,7 +21,7 @@ type Output struct {
 }
 
 type OpenIMClientVersion struct {
-	ClientVersion string `json:"clientVersion,omitempty" yaml:"clientVersion,omitempty"`	//sdk core version
+	ClientVersion string `json:"clientVersion,omitempty" yaml:"clientVersion,omitempty"` //sdk core version
 }
 
 // String returns info as a human-friendly version string.
