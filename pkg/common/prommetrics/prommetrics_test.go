b416e3fac3517208d5abc249eee769fb3cdb45cf
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-11-10 11:04:46 +0800

    fix: fix tim file rename

--- pkg/common/prommetrics/prommetrics_test.go
+++ pkg/common/prommetrics/prommetrics_test.go
@@ -1,80 +1,60 @@
 package prommetrics
 
 import (
-	"reflect"
 	"testing"
 
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
-	"github.com/openimsdk/open-im-server/v3/pkg/common/ginprometheus"
+	config2 "github.com/openimsdk/open-im-server/v3/pkg/common/config"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewGrpcPromObj(t *testing.T) {
-	type args struct {
-		cusMetrics []prometheus.Collector
+	// Create a custom metric to pass into the NewGrpcPromObj function.
+	customMetric := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "test_metric",
+		Help: "This is a test metric.",
+	})
+	cusMetrics := []prometheus.Collector{customMetric}
+
+	// Call NewGrpcPromObj with the custom metrics.
+	reg, grpcMetrics, err := NewGrpcPromObj(cusMetrics)
+
+	// Assert no error was returned.
+	assert.NoError(t, err)
+
+	// Assert the registry was correctly initialized.
+	assert.NotNil(t, reg)
+
+	// Assert the grpcMetrics was correctly initialized.
+	assert.NotNil(t, grpcMetrics)
+
+	// Assert that the custom metric is registered.
+	mfs, err := reg.Gather()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, mfs) // Ensure some metrics are present.
+	found := false
+	for _, mf := range mfs {
+		if *mf.Name == "test_metric" {
+			found = true
+			break
+		}
 	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *prometheus.Registry
-		want1   *grpc_prometheus.ServerMetrics
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := NewGrpcPromObj(tt.args.cusMetrics)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewGrpcPromObj() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewGrpcPromObj() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("NewGrpcPromObj() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
+	assert.True(t, found, "Custom metric not found in registry")
 }
 
 func TestGetGrpcCusMetrics(t *testing.T) {
-	type args struct {
-		registerName string
-	}
-	tests := []struct {
-		name string
-		args args
-		want []prometheus.Collector
+	// Test various cases based on the switch statement in the GetGrpcCusMetrics function.
+	testCases := []struct {
+		name     string
+		expected int // The expected number of metrics for each case.
 	}{
-		// TODO: Add test cases.
+		{config2.Config.RpcRegisterName.OpenImMessageGatewayName, 1},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetGrpcCusMetrics(tt.args.registerName); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetGrpcCusMetrics() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestGetGinCusMetrics(t *testing.T) {
-	type args struct {
-		name string
-	}
-	tests := []struct {
-		name string
-		args args
-		want []*ginprometheus.Metric
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetGinCusMetrics(tt.args.name); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetGinCusMetrics() = %v, want %v", got, tt.want)
-			}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			metrics := GetGrpcCusMetrics(tc.name)
+			assert.Len(t, metrics, tc.expected)
 		})
 	}
 }
