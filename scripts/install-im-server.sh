2d2a266b7412ae14ba338bd4ac4bfc1072967d5f
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-12-05 22:32:51 +0800

    fix: remove docker compose mysql
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

--- scripts/install-im-server.sh
+++ scripts/install-im-server.sh
@@ -40,10 +40,10 @@ else
     DOCKER_COMPOSE_COMMAND="docker-compose"
 fi
 
+"${OPENIM_ROOT}"/scripts/init-config.sh
+
 pushd "${OPENIM_ROOT}"
 ${DOCKER_COMPOSE_COMMAND} stop
-curl https://gitee.com/openimsdk/openim-docker/raw/main/example/full-openim-server-and-chat.yml -o docker-compose.yml
-"${OPENIM_ROOT}"/scripts/init-config.sh
 ${DOCKER_COMPOSE_COMMAND} up --remove-orphans -d
 sleep 60
 ${DOCKER_COMPOSE_COMMAND} logs openim-server
