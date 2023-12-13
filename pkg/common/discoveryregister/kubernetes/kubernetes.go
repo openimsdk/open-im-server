d61495b047a5e649c99e9845f8ab06577462e715
Xinwei Xiong (cubxxw) <3293172751nss@gmail.com>
2023-12-13 20:14:45 +0800

    feat: add zk and redis mongo env
    Signed-off-by: Xinwei Xiong (cubxxw) <3293172751nss@gmail.com>

---
+++ pkg/common/discoveryregister/kubernetes/kubernetes.go
@@ -0,0 +1,90 @@
+package kubernetes
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+
+	"github.com/OpenIMSDK/tools/discoveryregistry"
+)
+
+// K8sDR represents the Kubernetes service discovery and registration client.
+type K8sDR struct {
+	options         []grpc.DialOption
+	rpcRegisterAddr string
+}
+
+// NewK8sDiscoveryRegister creates a new instance of K8sDR for Kubernetes service discovery and registration.
+func NewK8sDiscoveryRegister() (discoveryregistry.SvcDiscoveryRegistry, error) {
+
+	return &K8sDR{}, nil
+}
+
+// Register registers a service with Kubernetes.
+func (cli *K8sDR) Register(serviceName, host string, port int, opts ...grpc.DialOption) error {
+	cli.rpcRegisterAddr = serviceName
+
+	return nil
+}
+
+// UnRegister removes a service registration from Kubernetes.
+func (cli *K8sDR) UnRegister() error {
+
+	return nil
+}
+
+// CreateRpcRootNodes creates root nodes for RPC in Kubernetes.
+func (cli *K8sDR) CreateRpcRootNodes(serviceNames []string) error {
+
+	return nil
+}
+
+// RegisterConf2Registry registers a configuration to the registry.
+func (cli *K8sDR) RegisterConf2Registry(key string, conf []byte) error {
+
+	return nil
+}
+
+// GetConfFromRegistry retrieves a configuration from the registry.
+func (cli *K8sDR) GetConfFromRegistry(key string) ([]byte, error) {
+	return nil, nil
+}
+
+// GetConns returns a list of gRPC client connections for a given service.
+func (cli *K8sDR) GetConns(ctx context.Context, serviceName string, opts ...grpc.DialOption) ([]*grpc.ClientConn, error) {
+	conn, err := grpc.DialContext(ctx, serviceName, append(cli.options, opts...)...)
+	return []*grpc.ClientConn{conn}, err
+}
+
+// GetConn returns a single gRPC client connection for a given service.
+func (cli *K8sDR) GetConn(ctx context.Context, serviceName string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	return grpc.DialContext(ctx, serviceName, append(cli.options, opts...)...)
+}
+
+// GetSelfConnTarget returns the connection target of the client itself.
+func (cli *K8sDR) GetSelfConnTarget() string {
+	return cli.rpcRegisterAddr
+}
+
+// AddOption adds gRPC dial options to the client.
+func (cli *K8sDR) AddOption(opts ...grpc.DialOption) {
+	cli.options = append(cli.options, opts...)
+}
+
+// CloseConn closes a given gRPC client connection.
+func (cli *K8sDR) CloseConn(conn *grpc.ClientConn) {
+	conn.Close()
+}
+
+// do not use this method for call rpc
+func (cli *K8sDR) GetClientLocalConns() map[string][]*grpc.ClientConn {
+	fmt.Println("should not call this function!!!!!!!!!!!!!!!!!!!!!!!!!")
+	return nil
+}
+
+// Close closes the K8sDR client.
+func (cli *K8sDR) Close() {
+	// Close any open resources here (if applicable)
+	return
+}
---
+++ pkg/common/discoveryregister/zookeeper/zookeeper.go
@@ -0,0 +1,46 @@
+package zookeeper
+
+import (
+	"os"
+	"strings"
+	"time"
+
+	"github.com/OpenIMSDK/tools/discoveryregistry"
+	openkeeper "github.com/OpenIMSDK/tools/discoveryregistry/zookeeper"
+	"github.com/OpenIMSDK/tools/log"
+	"github.com/openimsdk/open-im-server/v3/pkg/common/config"
+)
+
+// NewZookeeperDiscoveryRegister creates a new instance of ZookeeperDR for Zookeeper service discovery and registration.
+func NewZookeeperDiscoveryRegister() (discoveryregistry.SvcDiscoveryRegistry, error) {
+	schema := getEnv("ZOOKEEPER_SCHEMA", config.Config.Zookeeper.Schema)
+	zkAddr := getZkAddrFromEnv(config.Config.Zookeeper.ZkAddr)
+	username := getEnv("ZOOKEEPER_USERNAME", config.Config.Zookeeper.Username)
+	password := getEnv("ZOOKEEPER_PASSWORD", config.Config.Zookeeper.Password)
+
+	return openkeeper.NewClient(
+		zkAddr,
+		schema,
+		openkeeper.WithFreq(time.Hour),
+		openkeeper.WithUserNameAndPassword(username, password),
+		openkeeper.WithRoundRobin(),
+		openkeeper.WithTimeout(10),
+		openkeeper.WithLogger(log.NewZkLogger()),
+	)
+}
+
+// getEnv returns the value of an environment variable if it exists, otherwise it returns the fallback value.
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
+
+// getZkAddrFromEnv returns the value of an environment variable if it exists, otherwise it returns the fallback value.
+func getZkAddrFromEnv(fallback []string) []string {
+	if value, exists := os.LookupEnv("ZOOKEEPER_ADDRESS"); exists {
+		return strings.Split(value, ",")
+	}
+	return fallback
+}
