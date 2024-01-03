587533df4dc29b2c4921259c03b3adee0107b0eb
Brabem <69128477+luhaoling@users.noreply.github.com>
2024-01-03 15:46:38 +0800

    fix: update Notification update resp  (#1663)
    * fix: fix the error
    
    * fix: fix the SearchNotificationAccount resp
    
    * fix: fix the god
    
    * Update install-im-server.sh
    
    * Update install-im-server.sh
    
    ---------
    
    Co-authored-by: Xinwei Xiong <3293172751@qq.com>

--- internal/rpc/user/user.go
+++ internal/rpc/user/user.go
@@ -558,7 +558,7 @@ func (s *userServer) userModelToResp(users []*relation.UserModel) *pbuser.Search
 	accounts := make([]*pbuser.NotificationAccountInfo, 0)
 	var total int64
 	for _, v := range users {
-		if v.AppMangerLevel == constant.AppNotificationAdmin {
+		if v.AppMangerLevel == constant.AppNotificationAdmin && !utils.IsContain(v.UserID, config.Config.IMAdmin.UserID) {
 			temp := &pbuser.NotificationAccountInfo{
 				UserID:   v.UserID,
 				FaceURL:  v.FaceURL,
