3e30e50a09aa984a563dcfde1de0c91ec689c6d3
withchao <993506633@qq.com>
2024-01-15 11:42:18 +0800

    feat: local cache

--- pkg/localcache/local/callback.go
+++ pkg/localcache/local/callback.go
@@ -1,5 +1,1 @@
 package local
-
-import "github.com/hashicorp/golang-lru/v2/simplelru"
-
-type EvictCallback[K comparable, V any] simplelru.EvictCallback[K, V]
--- pkg/localcache/local/lru.go
+++ pkg/localcache/local/lru.go
@@ -1,108 +1,20 @@
 package local
 
-import (
-	"github.com/hashicorp/golang-lru/v2/simplelru"
-	"sync"
-	"time"
-)
+import "github.com/hashicorp/golang-lru/v2/simplelru"
 
-type LRU1[K comparable, V any] interface {
+type EvictCallback[K comparable, V any] simplelru.EvictCallback[K, V]
+
+type LRU[K comparable, V any] interface {
 	Get(key K, fetch func() (V, error)) (V, error)
 	Del(key K) bool
 	Stop()
 }
 
-//type expirableLRU[K comparable, V any] struct {
-//	core expirable.LRU[K, V]
-//}
-//
-//func (x *expirableLRU[K, V]) Get(key K, fetch func() (V, error)) (V, error) {
-//
-//	return x.core.Get(key, fetch)
-//}
-//
-//func (x *expirableLRU[K, V]) Del(key K) bool {
-//	return x.core.Remove(key)
-//}
-//
-//func (x *expirableLRU[K, V]) Stop() {
-//
-//}
+type Target interface {
+	IncrGetHit()
+	IncrGetSuccess()
+	IncrGetFailed()
 
-type waitItem[V any] struct {
-	lock    sync.Mutex
-	expires int64
-	err     error
-	value   V
-}
-
-func NewLRU[K comparable, V any](size int, successTTL, failedTTL time.Duration, target Target, onEvict EvictCallback[K, V]) *LRU[K, V] {
-	var cb simplelru.EvictCallback[K, *waitItem[V]]
-	if onEvict != nil {
-		cb = func(key K, value *waitItem[V]) {
-			onEvict(key, value.value)
-		}
-	}
-	core, err := simplelru.NewLRU[K, *waitItem[V]](size, cb)
-	if err != nil {
-		panic(err)
-	}
-	return &LRU[K, V]{
-		core:       core,
-		successTTL: successTTL,
-		failedTTL:  failedTTL,
-		target:     target,
-	}
-}
-
-type LRU[K comparable, V any] struct {
-	lock       sync.Mutex
-	core       *simplelru.LRU[K, *waitItem[V]]
-	successTTL time.Duration
-	failedTTL  time.Duration
-	target     Target
-}
-
-func (x *LRU[K, V]) Get(key K, fetch func() (V, error)) (V, error) {
-	x.lock.Lock()
-	v, ok := x.core.Get(key)
-	if ok {
-		x.lock.Unlock()
-		v.lock.Lock()
-		expires, value, err := v.expires, v.value, v.err
-		if expires != 0 && expires > time.Now().UnixMilli() {
-			v.lock.Unlock()
-			x.target.IncrGetHit()
-			return value, err
-		}
-	} else {
-		v = &waitItem[V]{}
-		x.core.Add(key, v)
-		v.lock.Lock()
-		x.lock.Unlock()
-	}
-	defer v.lock.Unlock()
-	if v.expires > time.Now().UnixMilli() {
-		return v.value, v.err
-	}
-	v.value, v.err = fetch()
-	if v.err == nil {
-		v.expires = time.Now().Add(x.successTTL).UnixMilli()
-		x.target.IncrGetSuccess()
-	} else {
-		v.expires = time.Now().Add(x.failedTTL).UnixMilli()
-		x.target.IncrGetFailed()
-	}
-	return v.value, v.err
-}
-
-func (x *LRU[K, V]) Del(key K) bool {
-	x.lock.Lock()
-	ok := x.core.Remove(key)
-	x.lock.Unlock()
-	return ok
-}
-
-func (x *LRU[K, V]) Stop() {
-
+	IncrDelHit()
+	IncrDelNotFound()
 }
