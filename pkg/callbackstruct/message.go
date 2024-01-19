907104701aa0a6f36aa9929848886dc76b8b8392
Brabem <69128477+luhaoling@users.noreply.github.com>
2024-01-19 23:50:33 +0800

    fix: change the afterSendSingleMsg config and fix CallbackSingleMsgRead unused problem (#1790)
    * fix: open the afterSendSingleMsg config
    
    * fix: fix the CallbackSingleMsgRead unused

--- pkg/callbackstruct/message.go
+++ pkg/callbackstruct/message.go
@@ -94,9 +94,10 @@ type CallbackGroupMsgReadResp struct {
 
 type CallbackSingleMsgReadReq struct {
 	CallbackCommand `json:"callbackCommand"`
-	SendID          string `json:"sendID"`
-	ReceiveID       string `json:"receiveID"`
-	ContentType     int64  `json:"contentType"`
+	ConversationID  string  `json:"conversationID"`
+	UserID          string  `json:"userID"`
+	Seqs            []int64 `json:"Seqs"`
+	ContentType     int32   `json:"contentType"`
 }
 
 type CallbackSingleMsgReadResp struct {
