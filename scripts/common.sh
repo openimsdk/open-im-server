01f2a886290e91b2a60d0fd55fcdc4bb78984e35
unknown <44203734@qq.com>
2024-03-08 22:24:58 +0800

    test
    Signed-off-by: unknown <44203734@qq.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+\Users\17191\GolandProjects\awesomeProject>#!/usr/bin/env bash
 
 # Copyright © 2023 OpenIM. All rights reserved.
 #
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 # shellcheck disable=SC2034 # Variables sourced in other scripts.
 
