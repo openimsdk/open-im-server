3cb427a7a8cc76e965fe5a2da2f046cf933aa9f5
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-03-19 18:51:43 +0800

    refactor: unified naming for module startup functions.

--- pkg/common/db/cache/msg.go
+++ pkg/common/db/cache/msg.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/OpenIMSDK/protocol/constant"
 	"github.com/OpenIMSDK/protocol/sdkws"
-	"github.com/go-redis/redis"
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/openimsdk/open-im-server/v3/pkg/common/config"
 	"github.com/openimsdk/open-im-server/v3/pkg/msgprocessor"
