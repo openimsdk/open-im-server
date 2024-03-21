a33dca049ec8db23d4688c9b9e55bdcea9a4d282
skiffer-git <44203734@qq.com>
2024-03-21 18:34:02 +0800

    Optimizing scripts

--- scripts/check-all.sh
+++ scripts/check-all.sh
@@ -86,7 +86,8 @@ else
 fi
 
 
-openim::log::status "Check OpenIM service:\n${OPENIM_OUTPUT_HOSTBIN}/openim-msgtransfer"
+openim::log::status "Check OpenIM service:"
+openim::log::colorless ${OPENIM_OUTPUT_HOSTBIN}/openim-msgtransfer"
 result=$(. $(dirname ${BASH_SOURCE})/install/openim-msgtransfer.sh openim::msgtransfer::check)
 if [[ $? -ne 0 ]]; then
   #echo "+++ cat openim log file >>> ${LOG_FILE}"
@@ -95,9 +96,9 @@ if [[ $? -ne 0 ]]; then
 fi
 
 
-openim::log::info "Check OpenIM service:"
+openim::log::status "Check OpenIM service:"
 for item in "${OPENIM_ALL_SERVICE_LIBRARIES_NO_TRANSFER[@]}"; do
-    echo "$item"
+    openim::log::colorless "$item"
 done
 
 result=$(openim::util::check_process_names ${OPENIM_ALL_SERVICE_LIBRARIES_NO_TRANSFER[@]})
