00a90003bea2c145cc811dc4d8d7cb95525c989e
Xinwei Xiong (cubxxw) <3293172751nss@gmail.com>
2024-04-01 17:32:24 +0800

    feat: add standardizer optimize makefile

--- scripts/make-rules/common-versions.mk
+++ scripts/make-rules/common-versions.mk
@@ -11,7 +11,41 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 # ==============================================================================
 # OpenIM Makefile Versions used
 #
+# Define the latest version for each tool to ensure consistent versioning across installations
+GOLANGCI_LINT_VERSION ?= latest
+GOIMPORTS_VERSION ?= latest
+ADDLICENSE_VERSION ?= latest
+DEEPCOPY_GEN_VERSION ?= latest
+CONVERSION_GEN_VERSION ?= latest
+GINKGO_VERSION ?= v1.16.2
+GO_GITLINT_VERSION ?= latest
+GO_JUNIT_REPORT_VERSION ?= latest
+GOTESTS_VERSION ?= latest
+SWAGGER_VERSION ?= latest
+KUBE_SCORE_VERSION ?= latest
+KUBECONFORM_VERSION ?= latest
+GSEMVER_VERSION ?= latest
+GIT_CHGLOG_VERSION ?= latest
+KO_VERSION ?= latest
+GITHUB_RELEASE_VERSION ?= latest
+COSCLI_VERSION ?= v0.19.0-beta
+MINIO_VERSION ?= latest
+DELVE_VERSION ?= latest
+AIR_VERSION ?= latest
+GOLINES_VERSION ?= latest
+GO_MOD_OUTDATED_VERSION ?= latest
+MOCKGEN_VERSION ?= latest
+PROTOC_GEN_GO_VERSION ?= latest
+CFSSL_VERSION ?= latest
+DEPTH_VERSION ?= latest
+GO_CALLVIS_VERSION ?= latest
+MISSPELL_VERSION ?= latest
+GOTHANKS_VERSION ?= latest
+RICHGO_VERSION ?= latest
+RTS_VERSION ?= latest
+TYPECHECK_VERSION ?= latest
+COMMENT_LANG_DETECTOR_VERSION ?= latest
+STANDARDIZER_VERSION ?= latest
