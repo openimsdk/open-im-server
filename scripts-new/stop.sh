1b45539b77a9f031d50e6f209d7255966a35ecfc
skiffer-git <44203734@qq.com>
2024-04-02 15:10:28 +0800

    Script Refactoring

--- scripts-new/stop.sh
+++ scripts-new/stop.sh
@@ -3,12 +3,20 @@
 
 
 
-OPENIM_ROOT=$(dirname "${BASH_SOURCE[0]}")/
-source "${OPENIM_ROOT}/lib/util.sh"
-source "${OPENIM_ROOT}/define/binaries.sh"
-source "${OPENIM_ROOT}/lib/path.sh"
 
 
 
+OPENIM_SCRIPTS=$(dirname "${BASH_SOURCE[0]}")/
+source "$OPENIM_SCRIPTS/bricks.sh"
 
 
+kill_exist_binaries
+
+result=$(check_binaries_stop)
+ret_val=$?
+
+if [ $ret_val -ne 0 ]; then
+  echo "$result"
+  echo "no stop..."
+  exit 1
+fi
