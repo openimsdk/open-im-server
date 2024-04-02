b9d44bad6ff3c56a2738bae22e4004a0ae9d7c77
skiffer-git <44203734@qq.com>
2024-04-02 18:05:46 +0800

    Script Refactoring

--- scripts-new/bricks.sh
+++ scripts-new/bricks.sh
@@ -35,6 +35,7 @@ start_binaries() {
 start_tools() {
   # Assume tool_binaries=("ncpu" "infra")
   for binary in "${tool_binaries[@]}"; do
+    echo $binary  bbbbbbbbbbbbbb
     local bin_full_path=$(get_tool_full_path "$binary")
     # Assuming get_tool_full_path defines full path for each tool
     cmd=("$bin_full_path" -c "$OPENIM_OUTPUT_CONFIG")
