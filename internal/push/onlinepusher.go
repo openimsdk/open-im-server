544bb96ef990dbbd32581cf77385671ca5364168
skiffer-git <44203734@qq.com>
2024-05-14 12:15:22 +0800

    Add etcd as a service discovery mechanism

--- internal/push/onlinepusher.go
+++ internal/push/onlinepusher.go
@@ -91,6 +91,7 @@ func (d *DefaultAllNode) GetConnsAndOnlinePush(ctx context.Context, msg *sdkws.M
 			msgClient := msggateway.NewMsgGatewayClient(conn)
 			reply, err := msgClient.SuperGroupOnlineBatchPushOneMsg(ctx, input)
 			if err != nil {
+				log.ZError(ctx, "SuperGroupOnlineBatchPushOneMsg ", err, input.String())
 				return nil
 			}
 
