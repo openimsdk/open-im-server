0899dd4a77bd6564df237ab4dba34ebaa99a2cbc
skiffer-git <44203734@qq.com>
2024-06-03 20:04:41 +0800

    Add a retry mechanism to mongo init

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -11,38 +11,56 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 mongosh <<EOF
-use admin
+var maxRetries = 300;
+var connected = false;
 var rootUsername = '$MONGO_INITDB_ROOT_USERNAME';
 var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-var authResult = db.auth(rootUsername, rootPassword);
-if (authResult) {
-  print('Authentication successful for root user: ' + rootUsername);
-} else {
-  print('Authentication failed for root user: ' + rootUsername + ' with password: ' + rootPassword);
-  quit(1);
-}
-
 var dbName = '$MONGO_INITDB_DATABASE';
-db = db.getSiblingDB(dbName);
 var openimUsername = '$MONGO_OPENIM_USERNAME';
 var openimPassword = '$MONGO_OPENIM_PASSWORD';
-var createUserResult = db.createUser({
-  user: openimUsername,
-  pwd: openimPassword,
-  roles: [
-    { role: 'readWrite', db: dbName }
-  ]
-});
 
-if (createUserResult.ok == 1) {
-  print('User creation successful. User: ' + openimUsername + ', Database: ' + dbName);
+while (!connected && maxRetries > 0) {
+    try {
+        db = connect('mongodb://127.0.0.1:27017/admin');
+        var authResult = db.auth(rootUsername, rootPassword);
+        if (authResult) {
+            print('Authentication successful for root user: ' + rootUsername);
+            connected = true;
+        } else {
+            print('Authentication failed for root user: ' + rootUsername + ' with password: ' + rootPassword);
+            quit(1);
+        }
+    } catch (e) {
+        maxRetries--;
+        print('Connection failed, retrying... Remaining attempts: ' + maxRetries);
+        sleep(1000); // Sleep for 1 second
+    }
+}
+
+if (connected) {
+    db = db.getSiblingDB(dbName);
+    var createUserResult = db.createUser({
+        user: openimUsername,
+        pwd: openimPassword,
+        roles: [{
+            role: 'readWrite',
+            db: dbName
+        }]
+    });
+
+    if (createUserResult.ok == 1) {
+        print('User creation successful. User: ' + openimUsername + ', Database: ' + dbName);
+    } else {
+        print('User creation failed for user: ' + openimUsername + ' in database: ' + dbName);
+        quit(1);
+    }
 } else {
-  print('User creation failed for user: ' + openimUsername + ' in database: ' + dbName);
-  quit(1);
+    print('Failed to connect to MongoDB after 300 retries.');
+    quit(1);
 }
 EOF
+
 
 
 
