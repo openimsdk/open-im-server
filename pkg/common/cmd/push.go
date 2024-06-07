19cabd9e6b2ea98f975dac318b8d74ab7c0e7cdb
icey-yu <1186114839@qq.com>
2024-06-07 19:22:06 +0800

    fix: config read

--- pkg/common/cmd/push.go
+++ pkg/common/cmd/push.go
@@ -19,6 +19,7 @@ import (
 	"github.com/openimsdk/open-im-server/v3/internal/push"
 	"github.com/openimsdk/open-im-server/v3/pkg/common/config"
 	"github.com/openimsdk/open-im-server/v3/pkg/common/startrpc"
+	"github.com/openimsdk/tools/system/program"
 	"github.com/spf13/cobra"
 )
 
@@ -43,6 +44,7 @@ func NewPushRpcCmd() *PushRpcCmd {
 		LocalCacheConfigFileName: &pushConfig.LocalCacheConfig,
 		DiscoveryConfigFilename:  &pushConfig.Discovery,
 	}
+	ret.RootCmd = NewRootCmd(program.GetProcessName(), WithConfigMap(ret.configMap))
 	ret.ctx = context.WithValue(context.Background(), "version", config.Version)
 	ret.Command.RunE = func(cmd *cobra.Command, args []string) error {
 		ret.pushConfig.FcmConfigPath = ret.ConfigPath()
