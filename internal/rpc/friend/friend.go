bd4fb8a1a0e1ec5cbd32b611174d73b9e8db57ab
withchao <993506633@qq.com>
2024-06-17 15:37:13 +0800

    merge

--- internal/rpc/friend/friend.go
+++ internal/rpc/friend/friend.go
@@ -51,11 +51,6 @@ type friendServer struct {
 	webhookClient         *webhook.Client
 }
 
-func (s *friendServer) GetFullFriendUserIDs(ctx context.Context, req *relation.GetFullFriendUserIDsReq) (*relation.GetFullFriendUserIDsResp, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
 type Config struct {
 	RpcConfig     config.Friend
 	RedisConfig   config.Redis
