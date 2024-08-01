5fb43eacac959d6c0474325d82840aabd0b07ae8
withchao <993506633@qq.com>
2024-08-01 10:56:32 +0800

    test

--- pkg/common/storage/cache/cachekey/online.go
+++ pkg/common/storage/cache/cachekey/online.go
@@ -5,7 +5,8 @@ import "time"
 const (
 	OnlineKey     = "ONLINE:"
 	OnlineChannel = "online_change"
-	OnlineExpire  = time.Hour / 2
+	//OnlineExpire  = time.Hour / 2
+	OnlineExpire = time.Minute / 2 // test
 )
 
 func GetOnlineKey(userID string) string {
