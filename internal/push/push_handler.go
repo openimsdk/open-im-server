3e7c10c95bdcb3058ccd9ef15c9e04a7a4a11f93
icey-yu <1186114839@qq.com>
2024-08-26 17:52:27 +0800

    feat: change log

--- internal/push/push_handler.go
+++ internal/push/push_handler.go
@@ -93,7 +93,7 @@ func (c *ConsumerHandler) handleMs2PsChat(ctx context.Context, msg []byte) {
 	nowSec := timeutil.GetCurrentTimestampBySecond()
 
 	if nowSec-sec > 10 {
-		log.ZWarn(ctx, "ignore push msg", nil, "msg", pbData.String(), "sec", sec, "nowSec", nowSec, "nowSec-sec", nowSec-sec)
+		log.ZWarn(ctx, "long time push msg", nil, "msg", pbData.String(), "sec", sec, "nowSec", nowSec, "nowSec-sec", nowSec-sec)
 	}
 	var err error
 	switch msgFromMQ.MsgData.SessionType {
