4f2b872e88b94b1798054e9608a4202709643e53
Monet Lee <monet_lee@163.com>
2024-10-29 10:00:34 +0800

    fix: improve time condition check mehtod.

--- internal/rpc/group/group.go
+++ internal/rpc/group/group.go
@@ -1219,7 +1219,7 @@ func (g *groupServer) TransferGroupOwner(ctx context.Context, req *pbgroup.Trans
 		}
 	}
 
-	if newOwner.MuteEndTime != time.Unix(0, 0) {
+	if !newOwner.MuteEndTime.IsZero() {
 		if _, err := g.CancelMuteGroupMember(ctx, &pbgroup.CancelMuteGroupMemberReq{
 			GroupID: group.GroupID,
 			UserID:  req.NewOwnerUserID}); err != nil {
