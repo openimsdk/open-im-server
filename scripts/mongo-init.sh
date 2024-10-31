5dbd7b00afab9e15b367400635d0f1c14fda4ef1
Monet Lee <monet_lee@163.com>
2024-10-31 16:40:39 +0800

    fix: improve mongo service deploy in docker.

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+echo "before running mongosh..."
+
 mongosh <<EOF
 var maxRetries = 300;
 var connected = false;
@@ -61,6 +63,4 @@ if (connected) {
 }
 EOF
 
-
-
-
+echo "mongosh completed..."
