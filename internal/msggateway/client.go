f185c981f764881d15354a0097793b251b44577a
withchao <993506633@qq.com>
2024-11-05 16:24:07 +0800

    feat: support text ping pong

--- internal/msggateway/client.go
+++ internal/msggateway/client.go
@@ -438,6 +438,9 @@ func (c *Client) handlerTextMessage(b []byte) error {
 		if err != nil {
 			return err
 		}
+		if err := c.conn.SetWriteDeadline(writeWait); err != nil {
+			return err
+		}
 		return c.conn.WriteMessage(MessageText, msgData)
 	default:
 		return fmt.Errorf("not support message type %s", msg.Type)
