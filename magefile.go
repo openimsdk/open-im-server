d77e58bddb0336ad379eca980e9ca04b32c44c48
Monet Lee <monet_lee@163.com>
2024-12-04 11:19:31 +0800

    build: implement services image build.

--- magefile.go
+++ magefile.go
@@ -4,14 +4,23 @@
 package main
 
 import (
-	"github.com/openimsdk/gomake/mageutil"
+	"flag"
 	"os"
+
+	"github.com/openimsdk/gomake/mageutil"
 )
 
 var Default = Build
 
 func Build() {
-	mageutil.Build()
+	flag.Parse()
+
+	bin := flag.Args()
+	if len(bin) != 0 {
+		bin = bin[1:]
+	}
+
+	mageutil.Build(bin)
 }
 
 func Start() {
