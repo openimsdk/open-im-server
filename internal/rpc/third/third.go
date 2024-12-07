29092c38e5b984f695887e14a27aaaaa6a088829
withchao <993506633@qq.com>
2024-12-07 11:36:02 +0800

    feat: support aws

--- internal/rpc/third/third.go
+++ internal/rpc/third/third.go
@@ -21,6 +21,8 @@ import (
 	"github.com/openimsdk/open-im-server/v3/pkg/common/storage/cache/redis"
 	"github.com/openimsdk/open-im-server/v3/pkg/common/storage/database/mgo"
 	"github.com/openimsdk/open-im-server/v3/pkg/localcache"
+	"github.com/openimsdk/tools/s3/aws"
+	"github.com/openimsdk/tools/s3/kodo"
 	"time"
 
 	"github.com/openimsdk/open-im-server/v3/pkg/common/storage/controller"
@@ -31,7 +33,6 @@ import (
 	"github.com/openimsdk/tools/discovery"
 	"github.com/openimsdk/tools/s3"
 	"github.com/openimsdk/tools/s3/cos"
-	"github.com/openimsdk/tools/s3/kodo"
 	"github.com/openimsdk/tools/s3/minio"
 	"github.com/openimsdk/tools/s3/oss"
 	"google.golang.org/grpc"
@@ -92,6 +93,8 @@ func Start(ctx context.Context, config *Config, client discovery.SvcDiscoveryReg
 		o, err = oss.NewOSS(*config.RpcConfig.Object.Oss.Build())
 	case "kodo":
 		o, err = kodo.NewKodo(*config.RpcConfig.Object.Kodo.Build())
+	case "aws":
+		o, err = aws.NewAws(*config.RpcConfig.Object.Aws.Build())
 	default:
 		err = fmt.Errorf("invalid object enable: %s", enable)
 	}
--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -15,6 +15,7 @@
 package config
 
 import (
+	"github.com/openimsdk/tools/s3/aws"
 	"strings"
 	"time"
 
@@ -290,14 +291,7 @@ type Third struct {
 		Cos    Cos    `mapstructure:"cos"`
 		Oss    Oss    `mapstructure:"oss"`
 		Kodo   Kodo   `mapstructure:"kodo"`
-		Aws    struct {
-			Endpoint        string `mapstructure:"endpoint"`
-			Region          string `mapstructure:"region"`
-			Bucket          string `mapstructure:"bucket"`
-			AccessKeyID     string `mapstructure:"accessKeyID"`
-			AccessKeySecret string `mapstructure:"accessKeySecret"`
-			PublicRead      bool   `mapstructure:"publicRead"`
-		} `mapstructure:"aws"`
+		Aws    Aws    `mapstructure:"aws"`
 	} `mapstructure:"object"`
 }
 type Cos struct {
@@ -323,6 +317,15 @@ type Kodo struct {
 	BucketURL       string `mapstructure:"bucketURL"`
 	AccessKeyID     string `mapstructure:"accessKeyID"`
 	AccessKeySecret string `mapstructure:"accessKeySecret"`
+	SessionToken    string `mapstructure:"sessionToken"`
+	PublicRead      bool   `mapstructure:"publicRead"`
+}
+
+type Aws struct {
+	Region          string `mapstructure:"region"`
+	Bucket          string `mapstructure:"bucket"`
+	AccessKeyID     string `mapstructure:"accessKeyID"`
+	SecretAccessKey string `mapstructure:"secretAccessKey"`
 	SessionToken    string `mapstructure:"sessionToken"`
 	PublicRead      bool   `mapstructure:"publicRead"`
 }
@@ -533,6 +536,7 @@ func (m *Minio) Build() *minio.Config {
 		SignEndpoint:    formatEndpoint(m.ExternalAddress),
 	}
 }
+
 func (c *Cos) Build() *cos.Config {
 	return &cos.Config{
 		BucketURL:    c.BucketURL,
@@ -564,6 +568,16 @@ func (o *Kodo) Build() *kodo.Config {
 		AccessKeySecret: o.AccessKeySecret,
 		SessionToken:    o.SessionToken,
 		PublicRead:      o.PublicRead,
+	}
+}
+
+func (o *Aws) Build() *aws.Config {
+	return &aws.Config{
+		Region:          o.Region,
+		Bucket:          o.Bucket,
+		AccessKeyID:     o.AccessKeyID,
+		SecretAccessKey: o.SecretAccessKey,
+		SessionToken:    o.SessionToken,
 	}
 }
 
