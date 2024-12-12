a8c11c557c62e36c12c776e41b8fa62ad440f7a2
Monet Lee <monet_lee@163.com>
2024-12-12 18:05:31 +0800

    remove unused method.

--- pkg/common/config/load_config.go
+++ pkg/common/config/load_config.go
@@ -41,23 +41,3 @@ func loadConfig(path string, envPrefix string, config any) error {
 	}
 	return nil
 }
-
-func loadConfigK8s(mountPath string, envPrefix string, config any) error {
-	// configFilePath := filepath.Join(mountPath, configFileName)
-	v := viper.New()
-	v.SetConfigFile(mountPath)
-	v.SetEnvPrefix(envPrefix)
-	v.AutomaticEnv()
-	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	if err := v.ReadInConfig(); err != nil {
-		return errs.WrapMsg(err, "failed to read config file", "path", mountPath, "envPrefix", envPrefix)
-	}
-
-	if err := v.Unmarshal(config, func(config *mapstructure.DecoderConfig) {
-		config.TagName = "mapstructure"
-	}); err != nil {
-		return errs.WrapMsg(err, "failed to unmarshal config", "path", mountPath, "envPrefix", envPrefix)
-	}
-	return nil
-}
