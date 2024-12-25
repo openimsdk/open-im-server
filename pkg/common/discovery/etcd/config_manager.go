541120dea657b44bc8db3a4c75c36acbd61b17c0
icey-yu <1186114839@qq.com>
2024-12-25 16:15:20 +0800

    fix: config

--- pkg/common/discovery/etcd/config_manager.go
+++ pkg/common/discovery/etcd/config_manager.go
@@ -2,7 +2,6 @@ package etcd
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
@@ -105,9 +104,6 @@ func restartServer(ctx context.Context) error {
 	if err != nil {
 		return errs.New("restart server fail").Wrap()
 	}
-	fmt.Println("::::::::::::::::::::::::::::::::::::::::::::::::::::")
-	fmt.Println(cmd.String())
-	fmt.Println("::::::::::::::::::::::::::::::::::::::::::::::::::::")
 
 	os.Exit(0)
 	return nil
