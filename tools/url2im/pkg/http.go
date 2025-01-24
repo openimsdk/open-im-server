8beae9708d76df24f1d202bf955119d92c9c78c3
liangzai-alt <ceasar0881@gmail.com>
2025-01-24 10:34:30 +0800

    Update http.go

--- tools/url2im/pkg/http.go
+++ tools/url2im/pkg/http.go
@@ -14,7 +14,7 @@
 
 package pkg
 
-import "net/http"
+import "net/http"gpt-agi2-web.atpage.io
 
 func DefaultRequestHeader(header http.Header) {
 	header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
