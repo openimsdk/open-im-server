268e1df1683bda527aa8182cbd89c158b7d8d91e
Monet Lee <monet_lee@163.com>
2025-03-11 10:56:20 +0800

    update test method args.

--- pkg/common/config/load_config_test.go
+++ pkg/common/config/load_config_test.go
@@ -10,7 +10,7 @@ import (
 func TestLoadLogConfig(t *testing.T) {
 	var log Log
 	os.Setenv("IMENV_LOG_REMAINLOGLEVEL", "5")
-	err := Load("../../../config/", "log.yml", "IMENV_LOG", "source", &log)
+	err := Load("../../../config/", "log.yml", "IMENV_LOG", &log)
 	assert.Nil(t, err)
 	t.Log(log.RemainLogLevel)
 	// assert.Equal(t, "../../../../logs/", log.StorageLocation)
@@ -22,7 +22,7 @@ func TestLoadMongoConfig(t *testing.T) {
 	os.Setenv("IMENV_MONGODB_PASSWORD", "openIM1231231")
 	// os.Setenv("IMENV_MONGODB_URI", "openIM123")
 	// os.Setenv("IMENV_MONGODB_USERNAME", "openIM123")
-	err := Load("../../../config/", "mongodb.yml", "IMENV_MONGODB", "source", &mongo)
+	err := Load("../../../config/", "mongodb.yml", "IMENV_MONGODB", &mongo)
 	// err := LoadApiConfig("../../../config/mongodb.yml", "IMENV_MONGODB", &mongo)
 
 	assert.Nil(t, err)
@@ -38,14 +38,14 @@ func TestLoadMongoConfig(t *testing.T) {
 
 func TestLoadMinioConfig(t *testing.T) {
 	var storageConfig Minio
-	err := Load("../../../config/minio.yml", "IMENV_MINIO", "", "source", &storageConfig)
+	err := Load("../../../config/minio.yml", "IMENV_MINIO", "", &storageConfig)
 	assert.Nil(t, err)
 	assert.Equal(t, "openim", storageConfig.Bucket)
 }
 
 func TestLoadWebhooksConfig(t *testing.T) {
 	var webhooks Webhooks
-	err := Load("../../../config/webhooks.yml", "IMENV_WEBHOOKS", "", "source", &webhooks)
+	err := Load("../../../config/webhooks.yml", "IMENV_WEBHOOKS", "", &webhooks)
 	assert.Nil(t, err)
 	assert.Equal(t, 5, webhooks.BeforeAddBlack.Timeout)
 
@@ -53,7 +53,7 @@ func TestLoadWebhooksConfig(t *testing.T) {
 
 func TestLoadOpenIMRpcUserConfig(t *testing.T) {
 	var user User
-	err := Load("../../../config/openim-rpc-user.yml", "IMENV_OPENIM_RPC_USER", "", "source", &user)
+	err := Load("../../../config/openim-rpc-user.yml", "IMENV_OPENIM_RPC_USER", "", &user)
 	assert.Nil(t, err)
 	//export IMENV_OPENIM_RPC_USER_RPC_LISTENIP="0.0.0.0"
 	assert.Equal(t, "0.0.0.0", user.RPC.ListenIP)
@@ -63,14 +63,14 @@ func TestLoadOpenIMRpcUserConfig(t *testing.T) {
 
 func TestLoadNotificationConfig(t *testing.T) {
 	var noti Notification
-	err := Load("../../../config/notification.yml", "IMENV_NOTIFICATION", "", "source", &noti)
+	err := Load("../../../config/notification.yml", "IMENV_NOTIFICATION", "", &noti)
 	assert.Nil(t, err)
 	assert.Equal(t, "Your friend's profile has been changed", noti.FriendRemarkSet.OfflinePush.Title)
 }
 
 func TestLoadOpenIMThirdConfig(t *testing.T) {
 	var third Third
-	err := Load("../../../config/openim-rpc-third.yml", "IMENV_OPENIM_RPC_THIRD", "", "source", &third)
+	err := Load("../../../config/openim-rpc-third.yml", "IMENV_OPENIM_RPC_THIRD", "", &third)
 	assert.Nil(t, err)
 	assert.Equal(t, "enabled", third.Object.Enable)
 	assert.Equal(t, "https://oss-cn-chengdu.aliyuncs.com", third.Object.Oss.Endpoint)
@@ -86,7 +86,7 @@ func TestLoadOpenIMThirdConfig(t *testing.T) {
 
 func TestTransferConfig(t *testing.T) {
 	var tran MsgTransfer
-	err := Load("../../../config/openim-msgtransfer.yml", "IMENV_OPENIM-MSGTRANSFER", "", "source", &tran)
+	err := Load("../../../config/openim-msgtransfer.yml", "IMENV_OPENIM-MSGTRANSFER", "", &tran)
 	assert.Nil(t, err)
 	assert.Equal(t, true, tran.Prometheus.Enable)
 	assert.Equal(t, true, tran.Prometheus.AutoSetPorts)
