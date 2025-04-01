fb7b203dfd1d37433404bdc89ce22d99314a6f8a
withchao <993506633@qq.com>
2025-04-01 16:43:40 +0800

    fix: the version number contains a line break

--- version/version.go
+++ version/version.go
@@ -1,6 +1,14 @@
 package version
 
-import _ "embed"
+import (
+	_ "embed"
+	"strings"
+)
 
 //go:embed version
 var Version string
+
+func init() {
+	Version = strings.Trim(Version, "\n")
+	Version = strings.TrimSpace(Version)
+}
