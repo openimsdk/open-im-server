bb4c44f786d4eea7a5d9d5fd14e098d45bbf034f
icey-yu <1186114839@qq.com>
2025-06-18 11:09:47 +0800

    fix: k8s discovery

--- pkg/common/discovery/discoveryregister.go
+++ pkg/common/discovery/discoveryregister.go
@@ -35,7 +35,8 @@ func NewDiscoveryRegister(discovery *config.Discovery, watchNames []string) (dis
 		return standalone.GetSvcDiscoveryRegistry(), nil
 	}
 	if runtimeenv.RuntimeEnvironment() == config.KUBERNETES {
-		return kubernetes.NewKubernetesConnManager(discovery.Kubernetes.Namespace,
+		return kubernetes.NewConnManager(discovery.Kubernetes.Namespace,
+			watchNames,
 			grpc.WithDefaultCallOptions(
 				grpc.MaxCallSendMsgSize(1024*1024*20),
 			),
